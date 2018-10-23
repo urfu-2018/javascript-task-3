1a8bc658b4e69ae626b286852380812cae11cb54
Ivan Smirnov <Van10000@mail.ru>
2018-10-23 21:11:20 +0500

    Solved

--- robbery.js
+++ robbery.js
@@ -4,7 +4,99 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-const isStar = true;
+exports.isStar = true;
+
+const timeRE = new RegExp('^(.{2})\\s(\\d{2}):(\\d{2})\\+(\\d)$');
+const dayNames = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+const minutesInDay = 1440;
+const minutesInHour = 60;
+const open = 0;
+const close = 1;
+let timeBeforeStart;
+let timeAfterEnd;
+
+function getMinutesFromWeekStart(timeString) {
+    const parsed = timeRE.exec(timeString);
+    const days = dayNames.indexOf(parsed[1]);
+    const hours = parseInt(parsed[2]) - parseInt(parsed[4]);
+    const minutes = parseInt(parsed[3]);
+
+    return days * minutesInDay + hours * minutesInHour + minutes;
+}
+
+function getEvents(schedule) {
+    return schedule.reduce((events, timePeriod) => {
+        events.push({ time: getMinutesFromWeekStart(timePeriod.from), type: open });
+        events.push({ time: getMinutesFromWeekStart(timePeriod.to), type: close });
+
+        return events;
+    }, []);
+}
+
+function getEventsFromRobberSchedule(robberSchedule) {
+    let result = robberSchedule.reduce((events, timePeriod) => {
+        events.push({ time: getMinutesFromWeekStart(timePeriod.from), type: close });
+        events.push({ time: getMinutesFromWeekStart(timePeriod.to), type: open });
+
+        return events;
+    }, [{ time: timeBeforeStart, type: open }]);
+    result.push({ time: timeAfterEnd, type: close });
+
+    return result;
+}
+
+function getEventsFromBankWorkingHours(bankWorkingHours) {
+    let bankWorkingHoursByDays = dayNames.slice(0, 3)
+        .map(day => {
+            return {
+                from: day + ' ' + bankWorkingHours.from,
+                to: day + ' ' + bankWorkingHours.to
+            };
+        });
+
+    return getEvents(bankWorkingHoursByDays);
+}
+
+// Use ScanLine algorithm
+// Don't really know how to simplify it, so just disable warning
+// eslint-disable-next-line complexity
+function getCommonMomentRanges(events, enoughOpenedForRobbery) {
+    events.sort((first, second) => {
+        let result = first.time - second.time;
+        if (result !== 0) {
+            return result;
+        }
+
+        return first.type - second.type;
+    });
+    let openedCount = 0;
+    const notStarted = timeBeforeStart - 1;
+    let rangeStarted = notStarted;
+    const ranges = [];
+    for (let event of events) {
+        openedCount += (event.type === open) ? 1 : -1;
+        if (openedCount >= enoughOpenedForRobbery && rangeStarted === notStarted) {
+            rangeStarted = event.time;
+        }
+        if (openedCount < enoughOpenedForRobbery && rangeStarted !== notStarted) {
+            ranges.push({ from: rangeStarted, to: event.time });
+            rangeStarted = notStarted;
+        }
+    }
+
+    return ranges;
+}
+
+function getDay(timeInMinutes) {
+    const dayNumber = Math.floor(timeInMinutes / minutesInDay);
+
+    return dayNames[dayNumber];
+}
+
+function formatNumber(number) {
+    return number < 10 ? '0' + number : number;
+}
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -14,8 +106,22 @@ const isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
-function getAppropriateMoment(schedule, duration, workingHours) {
+module.exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
+    const bankTimeZone = parseInt(workingHours.from[workingHours.from.length - 1]);
+    timeBeforeStart = getMinutesFromWeekStart('ПН 00:00+' + bankTimeZone);
+    timeAfterEnd = getMinutesFromWeekStart('ВС 23:59+' + bankTimeZone);
+    let bankEvents = getEventsFromBankWorkingHours(workingHours);
+    for (let key of Object.keys(schedule)) {
+        bankEvents = bankEvents.concat(getEventsFromRobberSchedule(schedule[key]));
+    }
+    const commonMomentRanges = getCommonMomentRanges(bankEvents, Object.keys(schedule).length + 1);
+    let robberyRanges = [];
+    for (let range of commonMomentRanges) {
+        if (range.to - range.from >= duration) {
+            robberyRanges.push({ from: range.from, to: range.to - duration });
+        }
+    }
 
     return {
 
@@ -24,7 +130,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return robberyRanges.length > 0;
         },
 
         /**
@@ -34,6 +140,17 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
+            if (robberyRanges.length === 0) {
+                return '';
+            }
+            const timeInBankTimezone = robberyRanges[0].from + bankTimeZone * minutesInHour;
+            const day = getDay(timeInBankTimezone);
+            const hour = formatNumber(Math.floor(timeInBankTimezone / 60) % 24);
+            const minute = formatNumber(timeInBankTimezone % 60);
+            template = template.replace('%DD', day);
+            template = template.replace('%HH', hour);
+            template = template.replace('%MM', minute);
+
             return template;
         },
 
@@ -43,13 +160,22 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+            if (robberyRanges.length === 0) {
+                return false;
+            }
+
+            const minimalTime = robberyRanges[0].from + 30;
+
+            for (let i = 0; i < robberyRanges.length; ++i) {
+                if (robberyRanges[i].to >= minimalTime) {
+                    robberyRanges = robberyRanges.slice(i);
+                    robberyRanges[0].from = Math.max(robberyRanges[0].from, minimalTime);
+
+                    return true;
+                }
+            }
+
             return false;
         }
     };
-}
-
-module.exports = {
-    getAppropriateMoment,
-
-    isStar
 };
