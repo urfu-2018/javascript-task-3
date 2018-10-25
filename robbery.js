1e1b94ffb54653b1279c4b66465836610f916aff
Daria <DaArtPetrova@mail.ru>
2018-10-25 22:11:45 +0500

    Delete comments

--- robbery.js
+++ robbery.js
@@ -15,10 +15,6 @@ const isStar = true;
  * @returns {Object}
  */
 
-// const bankTimeZone = getBankTimeZone(workingHours);
-// translateTimeRangesToStandard(schedule, bankTimeZone);
-// const freeTime = getFreeTime(schedule);
-
 function getAppropriateMoment(schedule, duration, workingHours) {
     // console.info(schedule, duration, workingHours);
     const bankTimeZone = getTimeZone(workingHours.from);
@@ -26,12 +22,6 @@ function getAppropriateMoment(schedule, duration, workingHours) {
     workSchedule = dateScheduleToMinutesSchedule(workSchedule, bankTimeZone);
     const freeTimeSchedule = {};
     Object.keys(schedule).forEach(function (key) {
-
-        /*
-        dateScheduleToMinutesSchedule(schedule[key], bankTimeZone);
-        freeTimeSchedule[key] = getFreeTimeSchedule(schedule[key]);
-        */
-
         const minutesSchedule = dateScheduleToMinutesSchedule(schedule[key], bankTimeZone);
         freeTimeSchedule[key] = getFreeTimeSchedule(minutesSchedule);
     });
@@ -233,110 +223,7 @@ function compareTimeRanges(a, b) {
     return -1;
 }
 
-/*
-const year = '2018';
-const month = '01';
-const weekDayToDateDay = { 'ПН': '01', 'ВТ': '02', 'СР': '03', 'ЧТ': '04', 'ПТ': '05',
-    'СБ': '06', 'ВС': '07' };
-
-function getWeekday(dateString) {
-    return dateString.substring(0, 2);
-}
-
-function getHours(dateString) {
-    return dateString.substring(3, 5);
-}
-
-function getMinutes(dateString) {
-    return dateString.substring(6, 8);
-}
-
-function getTimeZone(dateString) {
-    let timeZone = dateString.match(/\+\d{1,2}/)[0].substring(1);
-    if (timeZone.length === 1) {
-        timeZone = '0' + timeZone;
-    }
-
-    return timeZone;
-}
-
-function getFullDateString(day, hours, minutes, timeZone) {
-
-    // return `${year}-${month}-${day}T${hours}:${minutes}:00+${timeZone}00`;
-
-    return `${year}-${month}-${day}T${hours}:${minutes}:00.000+${timeZone}:00`;
-}
-
-function toDate(dateString, commonTimeZone) {
-    const day = weekDayToDateDay[getWeekday(dateString)];
-    const hours = getHours(dateString);
-    const minutes = getMinutes(dateString);
-    const timeZone = getTimeZone(dateString);
-    const fullDateString = getFullDateString(day, hours, minutes, timeZone);
-    const gmtMilliseconds = Date.parse(fullDateString);
-    // const withOffset = gmtMilliseconds + commonTimeZone * 1000 * 60 * 60;
-
-    return new Date(gmtMilliseconds);
-}
-
-function getBankTimeZone(bankWorkingHours) {
-    return parseInt(getTimeZone(bankWorkingHours.from));
-}
-
-function translateTimeRangesToStandard(schedule, commonTimeZone) {
-    Object.keys(schedule).forEach(function (key) {
-        schedule[key].forEach(function (range) {
-            range.from = toDate(range.from, commonTimeZone);
-            range.to = toDate(range.to, commonTimeZone);
-        });
-        schedule[key].sort(compareTimeRanges);
-    });
-}
-
-function compareTimeRanges(a, b) {
-    if (a.from >= b.from) {
-        return 1;
-    }
-
-    return -1;
-}
-
-function getFreeTime(schedule) {
-    const freeTime = {};
-    Object.keys(schedule).forEach(function (key) {
-        freeTime[key] = [];
-        let leftBorder = new Date(2018, 0, 1, 0, 0, 0);
-        schedule[key].forEach(function (range) {
-            let freeRange = {};
-            if (leftBorder !== range.from) {
-                if (leftBorder.getDay() !== range.from.getDay()) {
-                    while (leftBorder.getDay() !== range.from.getDay()) {
-                        const rightBorder = new Date(leftBorder);
-                        rightBorder.setHours(23);
-                        rightBorder.setMinutes(59);
-                        freeRange = { from: leftBorder, to: rightBorder };
-                        freeTime[key].push(freeRange);
-                        leftBorder = new Date(rightBorder.setMinutes(60));
-                    }
-                }
-                freeRange = { from: leftBorder, to: range.from };
-                freeTime[key].push(freeRange);
-            }
-            leftBorder = range.to;
-        });
-    });
-
-    return freeTime;
-}
-*/
-
 module.exports = {
     getAppropriateMoment,
     isStar
 };
-
-// Найти часовой пояс банка
-// Привести все временные промежутки к этому часовому поясу
-// Найти свободное время у всех грабителей
-// Обрезать свободное время по времени работы банка
-// Найти пересечения всех свободных времен грабитилей
