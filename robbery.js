35ec69a0df1ef53245b3e14d392130999c5c9c8c
Arseniy Garelyshev <ars.garelyshev@yandex.ru>
2018-10-25 13:59:39 +0500

    Finally passes tests!

--- robbery.js
+++ robbery.js
@@ -18,13 +18,14 @@ function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, '\n', duration, '\n', workingHours);
 
     const MINUTES_IN_DAY = 60 * 24;
-    const MINUTES_IN_WEEK = MINUTES_IN_DAY * 7;
+    // const MINUTES_IN_WEEK = MINUTES_IN_DAY * 7;
 
 
     const TARGET_TIME_ZONE = parseInt(workingHours.from.split('+')[1]);
 
     const weekEnum = Object.freeze({
-        'ПН': 0, 'ВТ': 1, 'СР': 2, 'ЧТ': 3, 'ПТ': 4, 'СБ': 5, 'ВС': 6
+        'ПН': 0, 'ВТ': 1, 'СР': 2, 'ЧТ': 3, 'ПТ': 4, 'СБ': 5, 'ВС': 6,
+        0: 'ПН', 1: 'ВТ', 2: 'СР' , 3: 'ЧТ', 4: 'ПТ', 5: 'СБ', 6: 'ВС'
     });
 
     /**
@@ -33,29 +34,33 @@ function getAppropriateMoment(schedule, duration, workingHours) {
      * @param {String} who
      * @returns {Object}
      */
-    function transformInterval(interval, who) {
+    function transformInterval(interval) {
         // 'ПН 09:35+5' => [ПН, 09, 35, 5]
         const regex = /^(.{2}) (\d{2}):(\d{2})+.(\d)+$/g;
+
+        // Для интервалов без даты, полагаем что с нуля (нужно для банка).
+        if (!interval.from.includes(' ')) {
+            interval.from = 'ПН ' + interval.from;
+        }
+
+        if (!interval.to.includes(' ')) {
+            interval.to = 'ПН ' + interval.to;
+        }
 
         // Да, я и сам думаю как вот ближайшие 5 вещей не писать два раза
         const intervalPartsFrom = interval.from.split(regex).filter(e => e.length > 0);
         const intervalPartsTo = interval.to.split(regex).filter(e => e.length > 0);
 
-        // Для интервалов без даты, полагаем что с нуля (нужно для банка).
-        if (intervalPartsFrom.length === 3) {
-            intervalPartsFrom.unshift('ПН');
-        }
-
         const dayFrom = weekEnum[interval.from.split(' ')[0]] * MINUTES_IN_DAY;
         const dayTo = weekEnum[interval.to.split(' ')[0]] * MINUTES_IN_DAY;
 
-        const timeFrom = parseInt(intervalPartsFrom[1]) * 24 + parseInt(intervalPartsFrom[2]);
-        const timeTo = parseInt(intervalPartsTo[1]) * 24 + parseInt(intervalPartsTo[2]);
+        const timeFrom = parseInt(intervalPartsFrom[1]) * 60 + parseInt(intervalPartsFrom[2]);
+        const timeTo = parseInt(intervalPartsTo[1]) * 60 + parseInt(intervalPartsTo[2]);
 
         const timezoneShift = (TARGET_TIME_ZONE - parseInt(intervalPartsFrom[3])) * 60;
 
         return { from: dayFrom + timeFrom + timezoneShift,
-            to: dayTo + timeTo + timezoneShift, who: who };
+            to: dayTo + timeTo + timezoneShift};
     }
 
     /**
@@ -63,57 +68,127 @@ function getAppropriateMoment(schedule, duration, workingHours) {
      * @param {Object} interval
      * @returns {Object[]}
      */
-    function invertInterval(interval) {
-
-        // инвертируем
-        let result = [];
-        result.push({ from: 0, to: interval.from, who: interval.who });
-        result.push({ from: interval.to, to: MINUTES_IN_WEEK, who: interval.who });
-
-        // убираем пустые интервалы
-        for (let i in result) {
-            if (result[i].to - result[i].from === 0) {
-                delete result[i];
-            }
-        }
-
-        return result;
-    }
+    // function invertInterval(interval) {
+    //
+    //     // инвертируем
+    //     let result = [];
+    //     result.push({ from: 0, to: interval.from, who: interval.who });
+    //     result.push({ from: interval.to, to: MINUTES_IN_WEEK, who: interval.who });
+    //
+    //     // убираем пустые интервалы
+    //     for (let i in result) {
+    //         if (result[i].to - result[i].from === 0) {
+    //             delete result[i];
+    //         }
+    //     }
+    //
+    //     return result;
+    // }
 
     /**
-     * Собирает все входные даdayнные в один массив (с преобразованиями)
-     * @returns {Object[]}
+     * Собирает все входные данные в один массив (с преобразованиями)
+     * @returns {Object}
      */
     function congregateIntervals() {
-        let availableTimes = []; // когда доступны грабители и банк
+        let bankTimes = [];
+        let busyTimes = [];
 
-        const transformed = transformInterval(workingHours, 'bank');
-        for (let i = 0; i < weekEnum.length; i ++) { // формируем расписание банка на неделю
-            availableTimes.push({
+        const transformed = transformInterval(workingHours);
+        for (let i = 0; i < 3; i ++) { // формируем расписание банка (3: до среды)
+            bankTimes.push({
                 from: transformed.from + MINUTES_IN_DAY * i,
-                to: transformed.to + MINUTES_IN_DAY * i,
-                who: transformed.who
+                to: transformed.to + MINUTES_IN_DAY * i
             });
         }
 
-        for (let [person, data] of Object.entries(schedule)) { // читаем расписание грабителей
+        for (let data of Object.values(schedule)) { // читаем расписание грабителей
             for (let interval of data) {
-                availableTimes = availableTimes
-                    .concat(invertInterval(transformInterval(interval, person)));
+                busyTimes.push(transformInterval(interval));
             }
         }
 
-        return availableTimes;
+        return { bank: bankTimes, people: busyTimes };
+    }
+
+    // /**
+    //  * Определяет, пересекаются ли два интервала.
+    //  * @param {Object} a
+    //  * @param {Object} b
+    //  * @returns {boolean}
+    //  */
+    // function twoIntervalsIntersect(a, b) {
+    //     return a.from <= b.from && a.to >= b.from || a.from <= b.to && a.to >= b.to;
+    // }
+
+    /**
+     * Определяет тип пересечения интервалов.
+     * 0 - не пересекаются,
+     * 1 - пересекаются и а начинаентся раньше b
+     * 2 - пересекаются и b начинается раньше а
+     * 3 - пересекаются и a вложен в b
+     * 4 - пересекаются и b вложен в a
+     * @param {Object} a
+     * @param {Object} b
+     * @return {Number}
+     */
+    function getIntersectionType(a, b) {
+        let intersectionType = 0;
+        if (a.from <= b.from && a.to >= b.from) {
+            intersectionType = 1;
+        }
+        if (a.from <= b.to && a.to >= b.to) {
+            intersectionType = 2;
+        }
+        if (a.from >= b.from && a.to <= b.to) {
+            return 3;
+        }
+        if (a.from <= b.from && a.to >= b.to) {
+            return 4;
+        }
+
+        return intersectionType;
     }
 
     /**
-     * Определяет, пересекаются ли два интервала.
+     * Производит вычитание интервалов (а - b)
      * @param {Object} a
      * @param {Object} b
-     * @returns {boolean}
+     * @returns {Object[]}
      */
-    function twoIntervalsIntersect(a, b) {
-        return a.from <= b.from && a.to >= b.from || a.from <= b.to && a.to >= b.to;
+    function subtractIntervals(a, b) {
+        switch (getIntersectionType(a, b)) {
+            case 0:
+                return [a];
+            case 1:
+                return [{
+                    from: Math.min(a.from, b.from),
+                    to: Math.max(a.from, b.from)
+                }];
+            case 2:
+                return [{
+                    from: Math.min(a.to, b.to),
+                    to: Math.max(a.to, b.to)
+                }];
+            case 3:
+                return [];
+            case 4:
+                return [{
+                    from: a.from,
+                    to: b.from }, {
+                    from: b.to,
+                    to: a.to }];
+        }
+    }
+
+    function intersectIntervals2electricBoogalo(schedule) {
+        for (let interval of schedule.people) {
+            let leftovers = [];
+            for (let day of schedule.bank) {
+                leftovers = leftovers.concat(subtractIntervals(day, interval));
+            }
+            schedule.bank = leftovers;
+        }
+        return schedule.bank;
     }
 
     /**
@@ -121,39 +196,46 @@ function getAppropriateMoment(schedule, duration, workingHours) {
      * @param {Object[]} intervals
      * @returns {Object[]}
      */
-    function intersectIntervals(intervals) {
-        let everyoneAvailable = [];
-        // Находим персечения
-        for (let i = 0; i < intervals.length - 1; i++) {
-            const a = intervals.splice(i, 1)[0];
-            if (a.who.split(';').length === 4) {
-                everyoneAvailable.push(a);
-                continue;
-            }
+    // function intersectIntervals(intervals) {
+    //     let everyoneAvailable = [];
+    //     // Находим персечения
+    //     for (let i = 0; i < intervals.length - 1; i++) {
+    //         const a = intervals.splice(i, 1)[0];
+    //         const b = intervals.splice(i, 1)[0];
+    //
+    //         if (a.who === b.who) {
+    //             continue;
+    //         }
+    //
+    //         if (a.who.split(';').length === 4) {
+    //             everyoneAvailable.push(a);
+    //             continue;
+    //         }
+    //
+    //         if (b.who.split(';').length === 4) {
+    //             everyoneAvailable.push(b);
+    //             intervals.push(a);
+    //             continue;
+    //         }
+    //
+    //         if (twoIntervalsIntersect(a, b)) {
+    //             const c = {
+    //                 from: Math.max(a.from, b.from),
+    //                 to: Math.min(a.to, b.to),
+    //                 who: a.who + ';' + b.who
+    //             };
+    //             intervals.push(c);
+    //         }
+    //         intervals.push(b);
+    //     }
+    //
+    //     return everyoneAvailable;
+    // }
 
-            const b = intervals.splice(i, 1)[0];
-            if (b.who.split(';').length === 4) {
-                everyoneAvailable.push(b);
-                continue;
-            }
-
-            if (twoIntervalsIntersect(a, b)) {
-                const c = {
-                    from: Math.max(a.from, b.from),
-                    to: Math.min(a.to, b.to),
-                    who: a.who + ';' + b.who
-                };
-                intervals.push(c);
-            }
-            intervals.push(b);
-        }
-
-        return everyoneAvailable;
-    }
-
-    const candidates = intersectIntervals(congregateIntervals())
-        .filter(interval => interval.to - interval.from > duration);
-    const firstCandidate = candidates.pop();
+    const candidates = intersectIntervals2electricBoogalo(congregateIntervals())
+            .filter(interval => (interval.to - interval.from) >= duration)
+            .sort((a, b) => (a.from > b.from));
+    let firstCandidate = candidates[0];
 
     return {
 
@@ -177,12 +259,18 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             }
 
             const time = firstCandidate.from;
-            const day = Math.floor(time / MINUTES_IN_DAY);
-            const hour = Math.floor((time % MINUTES_IN_DAY) / 24);
-            const minute = time % (MINUTES_IN_DAY * 24);
-            template.replace('%DD', day);
-            template.replace('%HH', hour);
-            template.replace('%MM', minute);
+            const day = weekEnum[Math.floor(time / MINUTES_IN_DAY)];
+            let hour = Math.floor((time % MINUTES_IN_DAY) / 60);
+            if (hour === 0) {
+                hour = '00';
+            }
+            let minute = (time % MINUTES_IN_DAY) % 60;
+            if (minute === 0) {
+                minute = '00';
+            }
+            template = template.replace('%DD', day);
+            template = template.replace('%HH', hour);
+            template = template.replace('%MM', minute);
 
             return template;
         },
@@ -193,7 +281,18 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return candidates.filter(e => e.from === firstCandidate.from + 30).length > 0;
+            const backup = firstCandidate;
+            if (firstCandidate.to - firstCandidate.from >= (30 + duration)) {
+                firstCandidate.from = firstCandidate.from + 30;
+            } else {
+                firstCandidate = candidates.filter(e => e.from >= firstCandidate.from + 30)[0];
+            }
+            if (firstCandidate === undefined) {
+                firstCandidate = backup;
+                return false;
+            }
+
+            return true;
         }
     };
 }
