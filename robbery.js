11c3c4eb183c568b4d97d6b0bba45d594245100d
Afanasev Leonid <leo-6809@mail.ru>
2018-10-26 07:41:27 +0500

    Моё решение задачи

--- robbery.js
+++ robbery.js
@@ -6,6 +6,31 @@
  */
 const isStar = true;
 
+const daysWeek=['ПН','ВТ','СР'];
+
+let fromTimes = {};
+let toTimes = {};
+
+let fromSuccessTimes = {};
+let toSuccessTimes = {};
+
+function createMinutesOfDay(hour, minute, timezone) {
+    return parseInt((parseInt(hour)+parseInt(timezone))*60+parseInt(minute));
+}
+
+function createSchedules(schedule) {
+    const keys=Object.keys(schedule);
+    keys.forEach(people=>{
+        schedule[people].forEach(participant => {
+            let time=participant.from.split(/\ |:|\+/);
+            fromTimes[people+time[0]]=createMinutesOfDay(time[1],time[2],time[3]);
+            time=participant.to.split(/\ |:|\+/);
+            toTimes[people+time[0]]=createMinutesOfDay(time[1],time[2],time[3]);
+            console.log(fromTimes[people+time[0]]);
+        });
+    });
+}
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -15,8 +40,22 @@ const isStar = true;
  * @returns {Object}
  */
 function getAppropriateMoment(schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    createSchedules(schedule);
+    let workingTime=workingHours.from.split(/\ |:|\+/);
+    const minuteFromWorkingBank=createMinutesOfDay(workingTime[0],workingTime[1],workingTime[2]);
+    workingTime=workingHours.to.split(/\ |:|\+/);
+    const minuteToWorkingBank=createMinutesOfDay(workingTime[0],workingTime[1],workingTime[2]);
 
+    daysWeek.forEach(dayWeek=>{
+        const keys=Object.keys(schedule);
+        keys.forEach(people=>{
+            if((duration+minuteFromWorkingBank)<fromTimes[people+daysWeek]||(toTimes[people+daysWeek]+duration)<minuteToWorkingBank){
+
+            }
+        });
+    });
+    console.log(fromTimes);
+    console.log(toTimes);
     return {
 
         /**
