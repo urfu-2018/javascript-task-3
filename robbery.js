6edfa18185e08365725bceb7585c7e2bdf14bb7c
Nikita Lebedev <acke208@yandex.ru>
2018-10-27 20:43:01 +0500

    done

--- robbery.js
+++ robbery.js
@@ -6,6 +6,9 @@
  */
 const isStar = true;
 
+const WEEK_DAYS = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+const ROBBERY_DAYS = ['ПН', 'ВТ', 'СР'];
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -16,6 +19,14 @@ const isStar = true;
  */
 function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
+    const scheduleForEachDay = fillActionDays(normalizeSchedule(schedule, workingHours));
+    const availableTime = {};
+    ROBBERY_DAYS.forEach(day => {
+        availableTime[day] = [...findRobberyTime(scheduleForEachDay[day], duration, workingHours)];
+    });
+    const startTimesGenerator = startTimeGenerator(getStartTimes(availableTime));
+    const timeExist = startTimesGenerator.next();
+    let startTime = timeExist.value;
 
     return {
 
@@ -24,7 +35,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return !timeExist.done;
         },
 
         /**
@@ -34,7 +45,21 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (!startTime) {
+                return '';
+            }
+            let hours = startTime.startHours;
+            let minutes = startTime.startMinutes;
+            let startDay = startTime.day;
+
+            if (minutes.length <= 1) {
+                minutes = '0' + minutes;
+            }
+
+
+            return template.replace('%HH', hours)
+                .replace('%MM', minutes)
+                .replace('%DD', startDay);
         },
 
         /**
@@ -43,9 +68,222 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            let next = startTimesGenerator.next();
+            if (!next.done) {
+                startTime = next.value;
+            }
+
+            return !next.done;
         }
     };
+}
+
+function* startTimeGenerator(startTimes) {
+    for (let i = 0; i < startTimes.length; i++) {
+        yield startTimes[i];
+    }
+}
+
+
+function getStartTimes(availableTime) {
+    return ROBBERY_DAYS.reduce((acc, day) => {
+        if (availableTime.hasOwnProperty(day)) {
+            availableTime[day].forEach(time => {
+                acc.push({
+                    day: day,
+                    startHours: String(time.from.getHours()),
+                    startMinutes: String(time.from.getMinutes())
+                });
+            });
+        }
+
+        return acc;
+    }, []);
+}
+
+function findRobberyTime(scheduleOfDay, duration, workingHours) {
+    let availableTime = [];
+    availableTime.push({
+        from: new Date(0, 0, 0, getHours(workingHours.from),
+            getMinutes(workingHours.from)),
+        to: new Date(0, 0, 0, getHours(workingHours.to), getMinutes(workingHours.to))
+    });
+
+    scheduleOfDay.map(segment => {
+        return {
+            from: new Date(0, 0, 0, getHours(segment.from), getMinutes(segment.from)),
+            to: new Date(0, 0, 0, getHours(segment.to), getMinutes(segment.to))
+        };
+    }).forEach(busyTime => {
+        availableTime = availableTime.reduce((acc, time) => {
+            return [...acc, ...intersectAvailableAndBusyTime(time, busyTime)];
+        }, []);
+    });
+
+    return availableTime.filter(time => {
+        return new Date(time.from.getTime() + duration * 60000).getTime() <=
+      time.to.getTime();
+    }).reduce((acc, time) => {
+        return [...acc, ...findLater(time, duration)];
+    }, []);
+}
+
+function findLater(time, duration) {
+    let laterTime = new Date(time.from.getTime() + 30 * 60000);
+    const laterTimes = [];
+    laterTimes.push(time);
+    while (new Date(laterTime.getTime() + duration * 60000).getTime() <= time.to.getTime()) {
+        laterTimes.push({
+            from: laterTime,
+            to: time.to
+        });
+        laterTime = new Date(laterTime.getTime() + 30 * 60000);
+    }
+
+    return laterTimes;
+}
+
+function intersectAvailableAndBusyTime(availableTime, busyTime) {
+    const separatedAvailableTime = [];
+    if (Math.min(availableTime.to.getTime(), busyTime.to.getTime()) -
+    Math.max(availableTime.from.getTime(), busyTime.from.getTime()) < 0) {
+        separatedAvailableTime.push(availableTime);
+
+        return separatedAvailableTime;
+    }
+    if (availableTime.from.getTime() >= busyTime.from.getTime()) {
+        if (availableTime.to.getTime() > busyTime.to.getTime()) {
+            availableTime.from = busyTime.to;
+            separatedAvailableTime.push(availableTime);
+        }
+    } else if (availableTime.to.getTime() <= busyTime.to.getTime()) {
+        availableTime.to = busyTime.from;
+        separatedAvailableTime.push(availableTime);
+    } else {
+        separatedAvailableTime.push({
+            from: availableTime.from,
+            to: busyTime.from
+        });
+        separatedAvailableTime.push({
+            from: busyTime.to,
+            to: availableTime.to
+        });
+    }
+
+    return separatedAvailableTime;
+}
+
+
+function fillActionDays(schedule) {
+    const filledDays = {};
+    ROBBERY_DAYS.forEach(day => {
+        filledDays[day] = [];
+    });
+    for (let name in schedule) {
+        if (schedule.hasOwnProperty(name)) {
+            schedule[name].forEach(segment => {
+                let day = getDay(segment.to);
+                filledDays[day].push(segment);
+            });
+        }
+    }
+
+    return filledDays;
+}
+
+function normalizeSchedule(schedule, workingHours) {
+    return Object.keys(schedule).reduce((acc, name) => {
+        if (schedule.hasOwnProperty(name)) {
+            acc[name] = equalizeShifts(schedule[name], getOffset(workingHours));
+            acc[name] = acc[name].reduce(separateSegments, []);
+
+            return acc;
+        }
+
+        return acc;
+    }, {});
+}
+
+function equalizeShifts(scheduleBlock, mainOffset) {
+    return scheduleBlock.map(segment => {
+        const offset = Number(getOffset(segment)) - Number(mainOffset);
+
+        return {
+            from: calculateNewDate(offset, segment.from),
+            to: calculateNewDate(offset, segment.to)
+        };
+    });
+}
+
+function calculateNewDate(offset, date) {
+    const time = new Date(0, 0, WEEK_DAYS.indexOf(getDay(date)), getHours(date), getMinutes(date));
+    const newTime = new Date(time.getTime() - offset * 3600000);
+
+    return `${WEEK_DAYS[newTime.getDay()]} ${newTime.getHours()}:${newTime.getMinutes()}`;
+}
+
+function separateSegment(segment) {
+    const separatedSegments = [];
+    let tempSegment = segment;
+    separatedSegments.push({
+        from: tempSegment.from.replace(/\+\d?\d/, ''),
+        to: getDay(tempSegment.from) + ' 23:59'
+    });
+    while (getDay(tempSegment.from) !== getDay(segment.to)) {
+        let nextDay = getAdjacentDays(getDay(tempSegment.from)).next;
+        tempSegment = nextDay === getDay(segment.to) ? {
+            from: nextDay + ' 00:00',
+            to: segment.to.replace(/\+\d?\d/, '')
+        } : {
+            from: nextDay + ' 00:00',
+            to: nextDay + ' 23:59'
+        };
+        separatedSegments.push(tempSegment);
+    }
+
+    return separatedSegments;
+}
+
+function getAdjacentDays(givenDay) {
+    return WEEK_DAYS.reduce((acc, day, index) => {
+        if (day === givenDay) {
+
+            return {
+                previous: WEEK_DAYS[index === 0 ? 6 : index - 1],
+                next: WEEK_DAYS[index === 6 ? 0 : index + 1]
+            };
+        }
+
+        return acc;
+    }, {});
+}
+
+function separateSegments(acc, segment) {
+    if (getDay(segment.from) !== getDay(segment.to)) {
+        return [...acc, ...separateSegment(segment)];
+    }
+
+    return [...acc, segment];
+}
+
+function getDay(time) {
+    return time.substr(0, 2);
+}
+
+function getTime(time) {
+    return /\d?\d:\d\d?/.exec(time)[0];
+}
+
+function getHours(time) {
+    return getTime(time).split(':')[0];
+}
+
+function getMinutes(time) {
+    return getTime(time).split(':')[1];
+}
+
+function getOffset(time) {
+    return /\d?\d$/.exec(time.to);
 }
 
 module.exports = {
