5c784b10abf17569201a359a51c9880215105093
Ropedann <ropedann@github.com>
2018-10-29 01:27:48 +0500

    Add javascript-task-3 attempt 4

--- robbery.js
+++ robbery.js
@@ -1,217 +1,187 @@
- 'use strict';
- 
- /**
-  * Сделано задание на звездочку
-  * Реализовано оба метода и tryLater
-  */
- const isStar = true;
- const days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
- const gangMembers = ['Danny', 'Rusty', 'Linus'];
- const dayValueInMin = 24 * 60;
- const startOfWeek = 0;
- const endOfWeek = dayValueInMin * 7 - 1;
- const robbingGraf = getrobbingGraf();
- 
- function getrobbingGraf() {
-     return days.slice(0, 3).map(day => {
-         return {
-             from: convToMin(`${day} 00:00+0`),
-             to: convToMin(`${day} 23:59+0`)
-         };
-     });
- }
- 
- function getMinFromWeekFirst(day) {
-     return days.indexOf(day) * dayValueInMin;
- }
- 
- function convToMin(str) {
-     const [dayOfWeek, hh, mm, timezone] = /([А-Я]{2}) (\d\d):(\d\d)\+(\d)/.exec(str).slice(1);
-     const minFromWeekStart = getMinFromWeekFirst(dayOfWeek);
-    const hours = parseInt(hh) - parseInt(timezone);
-    const min = parseInt(mm);
-     return minFromWeekStart + hours * 60 + min;
-    let result = minFromWeekStart + hours * 60 + min;
-    if (result < 0) {
-        return 0;
-    }
-     return result;
+'use strict';
+
+/**
+ * Сделано задание на звездочку
+ * Реализовано оба метода и tryLater
+ */
+const isStar = true;
+const DAYS_WORKS = ['ПН', 'ВТ', 'СР'];
+
+function getParsRegEx(gap) {
+    return /^(ПН|ВТ|СР|ЧТ|ПТ|СБ|ВС)?\s?([0-9]{2}):([0-9]{1,2})\+([0-9]{1,2})$/.exec(gap);
+
 }
- function parseTimePoints(schedule) {
-{
-        freeIntervals[gangMember].push(...parseTimePoints(schedule[gangMember]));
-        freeIntervals[gangMember].push(endOfWeek);
-        freeIntervals[gangMember] =
-            combineTimePoints(freeIntervals[gangMember].sort((a, b) => a > b));
-            combineTimePoints(fixTimeTable(freeIntervals[gangMember]));
+
+function getPartInterval(partGap) {
+    let timeGap = getParsRegEx(partGap);
+
+    return { day: timeGap[1], hours: Number(timeGap[2]),
+        minutes: Number(timeGap[3]), timeZone: Number(timeGap[4]) };
+}
+
+function getFullInterval(fullGap) {
+    return { from: getPartInterval(fullGap.from), to: getPartInterval(fullGap.to) };
+}
+
+function getParseFullInterval(fullInterval, timeZone) {
+    let mainTimeZone = timeZone ? timeZone - Number(fullInterval.from.timeZone) : 0;
+    let start = DAYS_WORKS.indexOf(fullInterval.from.day) * (24 * 60) +
+        (fullInterval.from.hours + mainTimeZone) * 60 +
+        fullInterval.from.minutes;
+    let end = DAYS_WORKS.indexOf(fullInterval.to.day) * (24 * 60) +
+        (fullInterval.to.hours + mainTimeZone) * 60 +
+        fullInterval.to.minutes;
+
+    return { start: start, end: end };
+}
+
+function checkInclusion(firstInterval, secondInterval) {
+    return firstInterval.start <= secondInterval.start && firstInterval.end >= secondInterval.end;
+}
+
+function isIntersection(firstInterval, secondInterval) {
+    return checkInclusion(firstInterval, secondInterval) ||
+        checkInclusion(secondInterval, firstInterval) ||
+        (firstInterval.start < secondInterval.start &&
+            firstInterval.end > secondInterval.start) ||
+        (firstInterval.end > secondInterval.end &&
+            firstInterval.start < secondInterval.end);
+}
+
+function chooseInterval(banksInterval, gangsInterval) {
+    if (checkInclusion(banksInterval, gangsInterval)) {
+        return [{ start: banksInterval.start, end: gangsInterval.start },
+            { start: gangsInterval.end, end: banksInterval.end }];
+    }
+    if (banksInterval.start <= gangsInterval.end &&
+        banksInterval.start >= gangsInterval.start) {
+        return [{ start: gangsInterval.end, end: banksInterval.end }];
+    }
+    if (banksInterval.end >= gangsInterval.start &&
+        banksInterval.end <= gangsInterval.end) {
+        return [{ start: banksInterval.start, end: gangsInterval.start }];
+    }
+}
+
+function allIntersect(banksInterval, gangsInterval) {
+    let intervalSchedule = [];
+    for (let bankInterval of banksInterval) {
+        if (!isIntersection(gangsInterval, bankInterval)) {
+            intervalSchedule.push(bankInterval);
+            continue;
+        }
+        if (checkInclusion(gangsInterval, bankInterval)) {
+            continue;
+        }
+        intervalSchedule = intervalSchedule.concat(chooseInterval(bankInterval, gangsInterval));
+    }
+
+    return intervalSchedule;
+}
+
+function getBanks(banksHours) {
+    return DAYS_WORKS.map(day => {
+        return {
+            from: { day: day, hours: banksHours.from.hours,
+                minutes: banksHours.from.minutes, timeZone: banksHours.from.timeZone
+            },
+            to: { day: day, hours: banksHours.to.hours,
+                minutes: banksHours.to.minutes, timeZone: banksHours.to.timeZone
+            }
+        };
+    });
+}
+
+
+/**
+ * @param {Object} schedule – Расписание Банды
+ * @param {Number} duration - Время на ограбление в минутах
+ * @param {Object} workingHours – Время работы банка
+ * @param {String} workingHours.from – Время открытия, например, "10:00+5"
+ * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
+ * @returns {Object}
+ */
+function getAppropriateMoment(schedule, duration, workingHours) {
+    let banksHours = getFullInterval(workingHours);
+    let banksZone = banksHours.to.timeZone;
+    let banksInterval = getBanks(banksHours);
+    let intervalsGangs = [];
+    for (let interval of banksInterval) {
+        intervalsGangs.push(getParseFullInterval(interval, banksZone));
+    }
+    let intervalsRobbers = [];
+    Object.keys(schedule).forEach((person) => {
+        schedule[person].forEach((interval) => {
+            intervalsRobbers.push(getParseFullInterval(getFullInterval(interval), banksZone));
+        });
+    });
+    intervalsRobbers.forEach(robbersInterval => {
+        intervalsGangs = allIntersect(intervalsGangs, robbersInterval);
+    });
+    intervalsGangs = intervalsGangs
+        .filter(interval => interval.end - interval.start >= duration);
+
+    return {
+
+        /**
+         * Найдено ли время
+         * @returns {Boolean}
+         */
+        exists: function () {
+            return intervalsGangs.length !== 0;
+        },
+
+        /**
+         * Возвращает отформатированную строку с часами для ограбления
+         * Например, "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
+         * @param {String} template
+         * @returns {String}
+         */
+        format: function (template) {
+            if (intervalsGangs.length === 0) {
+                return '';
+            }
+            let timeFinish = intervalsGangs[0].start;
+            let account = Math.floor(timeFinish / (24 * 60));
+            let day = DAYS_WORKS[account];
+            let minutes = timeFinish % (60);
+            let hours = Math.floor((timeFinish - account * 60 * 24) / 60);
+            let obj = { day, hours, minutes };
+            minutes = (obj.minutes >= 10 ? '' : '0') + obj.minutes;
+            hours = (obj.hours >= 10 ? '' : '0') + obj.hours;
+
+            return template
+                .replace('%HH', hours)
+                .replace('%MM', minutes)
+                .replace('%DD', obj.day);
+        },
+
+        /**
+         * Попробовать найти часы для ограбления позже [*]
+         * @star
+         * @returns {Boolean}
+         */
+        tryLater: function () {
+            if (intervalsGangs.length === 0) {
+                return false;
+            }
+            let nextMoment = 30 + intervalsGangs[0].start;
+            let postMoment = intervalsGangs.filter(interval => {
+                return nextMoment + duration <= interval.end;
+            });
+            if (postMoment.length !== 0) {
+                intervalsGangs = postMoment;
+                intervalsGangs[0].start = Math.max(
+                    intervalsGangs[0].start, nextMoment);
+            }
+
+            return postMoment.length !== 0;
+        }
     };
-     return freeIntervals;
 }
- function fixTimeTable(timePoints) {
-    if (timePoints[0] === 0 && timePoints[1] === 0) {
-        return timePoints.slice(1);
-    }
-     return timePoints;
-}
- function findAllIntersections(schedules, robbingDeadlines, workingHours, duration) {
-    let intersection = findIntersections(robbingDeadlines, workingHours, duration);
-      for (const gangMember in schedules) {
-         if (!schedules.hasOwnProperty(gangMember)) {
-             continue;
-         }
- 
-         intersection = findIntersections(schedules[gangMember], intersection, duration);
-         if (!intersection) {
-             return null;
-         }
- 
-     }
- 
-     return intersection;
- }
- 
- function findIntersections(scheduleOne, scheduleTwo, duration) {
-     let result = [];
-     scheduleOne.forEach(dateOne =>
-         scheduleTwo.forEach(dateTwo => {
-             const intersection = getMomentsCusp(dateOne, dateTwo, duration);
-             if (intersection) {
-                 result.push(intersection);
-             }
-         })
-     );
- 
-     return result.length === 0 ? null : result;
- }
- 
- function getMomentsCusp(dateOne, dateTwo, duration) {
-     if (dateOne.to < dateTwo.from || dateOne.from > dateTwo.to) {
-         return null;
-     }
- 
-     const left = Math.max(dateOne.from, dateTwo.from);
-     const right = Math.min(dateOne.to, dateTwo.to);
- 
-     if (Math.abs(right - left) < duration) {
-         return null;
-     }
- 
-     return {
-         from: left,
-         to: right
-     };
- }
- 
- function extractDayHoursMin(timeInMin) {
-     const dayIndex = Math.floor(timeInMin / dayValueInMin);
-     const day = days[dayIndex];
-     const dayMin = timeInMin % dayValueInMin;
-     const hours = Math.floor(dayMin / 60);
-     const min = dayMin - hours * 60;
- 
-     return [day, hours.toString(), min.toString()];
- }
- 
- function formatTemplate(template, day, hours, min) {
-     return template.replace('%DD', day)
-         .replace('%HH', hours.padStart(2, '0'))
-         .replace('%MM', min.padStart(2, '0'));
- }
- 
- function fillBankSchedule(bankWorkingHours) {
-     let bankSchedule = [];
-     days.forEach(day => {
-         bankSchedule.push({
-             from: convToMin(`${day} ${bankWorkingHours.from}`),
-             to: convToMin(`${day} ${bankWorkingHours.to}`)
-         });
-     });
- 
-     return bankSchedule;
- }
- 
- /**
-  * @param {Object} schedule – Расписание Банды
-  * @param {Number} duration - Время на ограбление в минутах
-  * @param {Object} workingHours – Время работы банка
-  * @param {String} workingHours.from – Время открытия, например, "10:00+5"
-  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
-  * @returns {Object}
-  */
- function getAppropriateMoment(schedule, duration, workingHours) {
-     console.info(schedule, duration, workingHours);
-     const bankTimezone = parseInt(workingHours.from.slice(6));
- 
-     const gangSchedule = getGangFreeTimeIntervals(schedule);
-     const bankSchedule = fillBankSchedule(workingHours);
- 
-     const intersections =
-         findAllIntersections(gangSchedule, robbingGraf, bankSchedule, duration);
- 
-     let pointer = 0;
- 
-     return {
- 
-         /**
-          * Найдено ли время
-          * @returns {Boolean}
-          */
-         exists: function () {
-             return intersections && intersections.length > 0;
-         },
- 
-         /**
-          * Возвращает отформатированную строку с часами для ограбления
-          * Например, "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
-          * @param {String} template
-          * @returns {String}
-          */
-         format: function (template) {
-             if (!this.exists()) {
-                 return '';
-             }
-             const currentRobberyInterval = intersections[pointer];
-             const startTime = currentRobberyInterval.from + bankTimezone * 60;
- 
-             return formatTemplate(template, ...extractDayHoursMin(startTime));
-         },
- 
-         /**
-          * Попробовать найти часы для ограбления позже [*]
-          * @star
-          * @returns {Boolean}
-          */
-         tryLater: function () {
-             if (!this.exists() || pointer === intersections.length) {
-                 return false;
-             }
- 
-             const halfOfAnHour = 30;
-             let currentDate = intersections[pointer];
- 
-             if (currentDate.from + halfOfAnHour + duration <= currentDate.to) {
-                 currentDate.from += halfOfAnHour;
- 
-                 return true;
-             }
- 
-             const nextIndex = intersections
-                 .slice(pointer + 1)
-                 .findIndex(date => date.from - currentDate.to > halfOfAnHour);
- 
-             if (nextIndex === -1) {
-                 return false;
-             }
-             pointer += nextIndex + 1;
- 
-             return true;
-         }
-     };
- }
- 
- module.exports = {
-     getAppropriateMoment,
- 
-     isStar
- };
- 
+
+module.exports = {
+    getAppropriateMoment,
+
+    isStar
+};
