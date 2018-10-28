81b046e28b6ade538feea0340346adb0f0c1d467
Eien_g <GSKryachko@gmail.com>
2018-10-28 11:57:04 +0500

    work in progress

--- robbery.js
+++ robbery.js
@@ -6,6 +6,12 @@
  */
 const isStar = true;
 
+const dayToHours = new Map(
+    [['ПН', 0],
+        ['ВТ', 24],
+        ['СР', 48]]
+);
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -46,6 +52,57 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             return false;
         }
     };
+}
+
+function convertToMinutesInBankTime(timestring, bankTime) {
+    let day = timestring.slice(0, 2);
+    let hours = parseInt(timestring.slice(3, 5)) + parseInt(timestring.slice(9)) - bankTime;
+    let minutes = parseInt(timestring.slice(6, 8));
+
+    return dayToHours[day] + hours * 60 + minutes;
+}
+
+function getCommonListOfBusyIntervals(schedule) {
+    const intervals = [];
+    for (let user in schedule) {
+        if (! schedule.hasOwnProperty(user)) {
+            continue;
+        }
+        for (let interval in schedule[user]) {
+            if (! schedule[user].hasOwnProperty(interval)) {
+                continue;
+            }
+            intervals.push([convertToMinutesInBankTime(interval.from), user, 'from']);
+            intervals.push([convertToMinutesInBankTime(interval.to), user, 'to']);
+        }
+    }
+    intervals.sort((a, b)=>a[0] - b[0]);
+}
+
+function getFreeTime(busyIntervals) {
+    const isOpen = new Map();
+    const resultIntervals = [];
+    let isUnionOpen = true;
+    resultIntervals.push([0, 'from']);
+    for (let i = 0; i < busyIntervals.length; i++) {
+        let time = busyIntervals[i];
+        isOpen[time[1]] = time[2] === 'from';
+        if (isOpen.every(x=>!x) && !isUnionOpen) {
+            resultIntervals.push([time[0], 'from']);
+            isUnionOpen = true;
+        } else if (isUnionOpen) {
+            resultIntervals.push([time[0], 'to']);
+            isUnionOpen = false;
+        }
+    }
+
+    return resultIntervals;
+}
+
+function getFreeTimeIntervals(userToBusyIntervals) {
+    for (let day = 0; day < 3; day++) {
+
+    }
 }
 
 module.exports = {
