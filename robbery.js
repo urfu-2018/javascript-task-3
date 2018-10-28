14e7aaa0ae25a605360114c44f0bae7ca3608348
petryashov <petryashov@skbkontur.ru>
2018-10-28 17:31:48 +0500

    init

--- robbery.js
+++ robbery.js
@@ -6,6 +6,97 @@
  */
 const isStar = true;
 
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+const MINUTES_IN_DAY = MINUTES_IN_HOUR * HOURS_IN_DAY;
+const DAYS_OF_WEEK = [
+    'ПН',
+    'ВТ',
+    'СР',
+    'ЧТ',
+    'ПТ',
+    'СБ',
+    'ВС'
+];
+const START_OF_WEEK = dateFactory(DAYS_OF_WEEK[0], 0, 0);
+const END_OF_WEEK = dateFactory(DAYS_OF_WEEK[DAYS_OF_WEEK.length - 1], 23, 59);
+const ROBBERY_TIME_INTERVAL = intervalFactory(
+    dateFactory('ПН', 0, 0),
+    dateFactory('СР', 23, 59)
+);
+
+function timeFactory(hours, minutes, zone) {
+    return {
+        hours,
+        minutes,
+        zone
+    };
+}
+
+function dateFactory(day, hours, minutes, zone) {
+    return {
+        day,
+        time: timeFactory(hours, minutes, zone)
+    };
+}
+
+function intervalFactory(from, to) {
+    return {
+        from,
+        to
+    };
+}
+
+function dateCompare(date1, date2) {
+    if (date1 === date2) {
+        return 0;
+    }
+
+    const daysCompare = DAYS_OF_WEEK.indexOf(date1.day) - DAYS_OF_WEEK.indexOf(date2.day);
+
+    if (daysCompare !== 0) {
+        return Math.sign(daysCompare);
+    }
+
+    return timeCompare(date1.time, date2.time);
+}
+
+function timeCompare(time1, time2) {
+    if (time1 === time2) {
+        return 0;
+    }
+
+    const hoursCompare = time1.hours - time2.hours;
+
+    if (hoursCompare !== 0) {
+        return Math.sign(hoursCompare);
+    }
+
+    return Math.sign(time1.minutes - time2.minutes);
+}
+
+function getBusyIntervals(bankWorkInterval, schedule, bankZone) {
+    const bankCloseIntervals = getCloseIntervals(bankWorkInterval);
+
+    return Object.values(schedule)
+        .reduce((periods1, periods2) => periods1.concat(periods2))
+        .map(period => {
+            return intervalFactory(
+                toDateWithCustomZone(period.from, bankZone),
+                toDateWithCustomZone(period.to, bankZone));
+        })
+        .concat(bankCloseIntervals)
+        .concat(invertIntervals([ROBBERY_TIME_INTERVAL]));
+}
+
+function getValidIntervals(busyIntervals, duration) {
+    const sortedBusyIntervals = busyIntervals
+        .sort((interval1, interval2) => dateCompare(interval1.from, interval2.from));
+
+    return invertIntervals(mergeIntervals(sortedBusyIntervals))
+        .filter(interval => intervalToMinutes(interval) >= duration);
+}
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -15,7 +106,15 @@ const isStar = true;
  * @returns {Object}
  */
 function getAppropriateMoment(schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    const bankWorkInterval = intervalFactory(
+        parseTimeWithZone(workingHours.from),
+        parseTimeWithZone(workingHours.to)
+    );
+
+    const bankTimeZone = bankWorkInterval.from.zone;
+
+    const busyIntervals = getBusyIntervals(bankWorkInterval, schedule, bankTimeZone);
+    let validInterval = getValidIntervals(busyIntervals, duration).shift();
 
     return {
 
@@ -24,7 +123,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return validInterval !== undefined;
         },
 
         /**
@@ -34,7 +133,17 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (validInterval === undefined) {
+                return '';
+            }
+
+            const validDate = validInterval.from;
+            const { hours, minutes } = validDate.time;
+
+            return template
+                .replace('%HH', hours < 10 ? '0' + hours : hours)
+                .replace('%MM', minutes < 10 ? '0' + minutes : minutes)
+                .replace('%DD', validDate.day);
         },
 
         /**
@@ -43,13 +152,139 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+            const newBusyInterval = intervalFactory(
+                validInterval.from,
+                addMinutes(validInterval.from, 30)
+            );
+            busyIntervals.push(newBusyInterval);
+            const newValidInterval = getValidIntervals(busyIntervals, duration).shift();
+            if (newValidInterval) {
+                validInterval = newValidInterval;
+
+                return true;
+            }
+
             return false;
         }
     };
 }
 
+function addMinutes(date, minutes) {
+    let newDateInMinutes = dateToMinutesFromStartOfWeek(date) + minutes;
+    const newDay = parseInt(newDateInMinutes / MINUTES_IN_DAY);
+    newDateInMinutes -= MINUTES_IN_DAY * newDay;
+    const newHour = parseInt(newDateInMinutes / MINUTES_IN_HOUR);
+    newDateInMinutes -= MINUTES_IN_HOUR * newHour;
+
+    return dateFactory(
+        DAYS_OF_WEEK[newDay],
+        newHour,
+        newDateInMinutes,
+        date.time.zone);
+}
+
+function dateToMinutesFromStartOfWeek(date) {
+    return DAYS_OF_WEEK.indexOf(date.day) * MINUTES_IN_DAY +
+        date.time.hours * MINUTES_IN_HOUR +
+        date.time.minutes;
+}
+
+function getCloseIntervals(workInterval) {
+    const { from, to } = workInterval;
+    const weekOpenIntervals = DAYS_OF_WEEK
+        .map(day => intervalFactory(
+            dateFactory(day, from.hours, from.minutes),
+            dateFactory(day, to.hours, to.minutes),
+        ));
+
+    return invertIntervals(weekOpenIntervals);
+}
+
+function intervalToMinutes(interval) {
+    const { from, to } = interval;
+    const days = DAYS_OF_WEEK.indexOf(to.day) - DAYS_OF_WEEK.indexOf(from.day);
+    const hours = to.time.hours - from.time.hours;
+    const minutes = to.time.minutes - from.time.minutes;
+
+    return days * MINUTES_IN_DAY + hours * MINUTES_IN_HOUR + minutes;
+}
+
+function mergeIntervals(intervals) {
+    let lastInterval = intervals[0];
+    const newIntervals = [];
+    for (let i = 1; i < intervals.length; i++) {
+        let newInterval = intervals[i];
+        if (dateCompare(lastInterval.to, newInterval.from) === -1) {
+            newIntervals.push(lastInterval);
+            lastInterval = newInterval;
+        } else {
+            lastInterval.to = dateCompare(lastInterval.to, newInterval.to) === -1
+                ? newInterval.to
+                : lastInterval.to;
+        }
+    }
+    newIntervals.push(lastInterval);
+
+    return newIntervals;
+}
+
+function invertIntervals(intervals) {
+    const newIntervals = [];
+    newIntervals.push(intervalFactory(
+        START_OF_WEEK,
+        intervals[0].from
+    ));
+    for (let i = 0; i < intervals.length - 1; i++) {
+        newIntervals.push(intervalFactory(
+            intervals[i].to,
+            intervals[i + 1].from,
+        ));
+    }
+    newIntervals.push(intervalFactory(
+        intervals[intervals.length - 1].to,
+        END_OF_WEEK
+    ));
+
+    return newIntervals;
+}
+
+function toDateWithCustomZone(dateString, newZone) {
+    const [day, timeWithZone] = dateString.split(' ');
+    const time = parseTimeWithZone(timeWithZone);
+    let newHours = time.hours - time.zone + newZone;
+    let newDay = newHours < 0 ? shiftDay(day, -1) : day;
+    newDay = newHours >= HOURS_IN_DAY ? shiftDay(day, 1) : newDay;
+    newHours = (newHours + HOURS_IN_DAY) % HOURS_IN_DAY;
+
+    return dateFactory(newDay, newHours, time.minutes, newZone);
+}
+
+function parseTimeWithZone(timeString) {
+    const dateRegex = /^(\d\d):(\d\d)\+(\d\d|\d)$/;
+    const [hours, minutes, zone] = dateRegex.exec(timeString).slice(1)
+        .map(x => parseInt(x));
+
+    return timeFactory(hours, minutes, zone);
+}
+
+function shiftDay(day, shift) {
+    shift = shift % DAYS_OF_WEEK.length + DAYS_OF_WEEK.length;
+    const newDayIndex = (DAYS_OF_WEEK.indexOf(day) + shift) % DAYS_OF_WEEK.length;
+
+    return DAYS_OF_WEEK[newDayIndex];
+}
+
 module.exports = {
     getAppropriateMoment,
-
+    toDateWithCustomZone,
+    mergeIntervals,
+    timeCompare,
+    dateCompare,
+    invertIntervals,
+    intervalToMinutes,
+    dateFactory,
+    intervalFactory,
+    timeFactory,
+    addMinutes,
     isStar
 };
--- robbery.spec.js
+++ robbery.spec.js
@@ -5,6 +5,11 @@ const assert = require('assert');
 
 const robbery = require('./robbery');
 
+const intervalFactory = robbery.intervalFactory;
+const dateFactory = robbery.dateFactory;
+const timeFactory = robbery.timeFactory;
+
+
 describe('robbery.getAppropriateMoment()', () => {
     function getMomentFor(time) {
         return robbery.getAppropriateMoment(
@@ -28,6 +33,211 @@ describe('robbery.getAppropriateMoment()', () => {
         );
     }
 
+    it('должен парсить дату c новой зоной', () => {
+        assert.deepStrictEqual(
+            robbery.toDateWithCustomZone('ПН 12:00+5', 0),
+            dateFactory('ПН', 7, 0, 0)
+        );
+
+        assert.deepStrictEqual(
+            robbery.toDateWithCustomZone('ПН 00:00+5', 0),
+            dateFactory('ВС', 19, 0, 0)
+        );
+
+        assert.deepStrictEqual(
+            robbery.toDateWithCustomZone('ПН 20:30+5', 0),
+            dateFactory('ПН', 15, 30, 0)
+        );
+
+        assert.deepStrictEqual(
+            robbery.toDateWithCustomZone('ПН 20:30+5', 7),
+            dateFactory('ПН', 22, 30, 7)
+        );
+
+        assert.deepStrictEqual(
+            robbery.toDateWithCustomZone('ПН 22:30+5', 7),
+            dateFactory('ВТ', 0, 30, 7)
+        );
+    });
+
+    it('должен сравнивать время', () => {
+        assert.deepStrictEqual(
+            robbery.timeCompare(
+                timeFactory(7, 0, 0),
+                timeFactory(7, 0, 0)
+            ), 0);
+
+        assert.deepStrictEqual(
+            robbery.timeCompare(
+                timeFactory(7, 0, 0),
+                timeFactory(6, 0, 0)
+            ), 1);
+
+        assert.deepStrictEqual(
+            robbery.timeCompare(
+                timeFactory(7, 0, 0),
+                timeFactory(7, 30, 0)
+            ), -1);
+    });
+
+    it('должен сравнивать даты', () => {
+        assert.deepStrictEqual(
+            robbery.dateCompare(
+                dateFactory('ВТ', 7, 0, 0),
+                dateFactory('ВТ', 7, 0, 0)
+            ), 0);
+        assert.deepStrictEqual(
+            robbery.dateCompare(
+                dateFactory('ВТ', 7, 0, 0),
+                dateFactory('ПН', 7, 0, 0)
+            ), 1);
+
+        assert.deepStrictEqual(
+            robbery.dateCompare(
+                dateFactory('ВТ', 7, 0, 0),
+                dateFactory('СР', 7, 0, 0)
+            ), -1);
+
+        assert.deepStrictEqual(
+            robbery.dateCompare(
+                dateFactory('ПН', 7, 0, 0),
+                dateFactory('ВС', 7, 0, 0)
+            ), -1);
+
+        assert.deepStrictEqual(
+            robbery.dateCompare(
+                dateFactory('ВС', 7, 0, 0),
+                dateFactory('ПН', 7, 0, 0)
+            ), 1);
+    });
+
+    it('должен объединять периоды', () => {
+        assert.deepStrictEqual(
+            robbery.mergeIntervals([
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0, 0),
+                    dateFactory('СР', 5, 0, 0),
+                ),
+                intervalFactory(
+                    dateFactory('СР', 3, 0, 0),
+                    dateFactory('ЧТ', 7, 0, 0),
+                )
+            ]
+            ), [
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0, 0),
+                    dateFactory('ЧТ', 7, 0, 0),
+                )
+            ]);
+
+        assert.deepStrictEqual(
+            robbery.mergeIntervals([
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0, 0),
+                    dateFactory('СР', 5, 0, 0),
+                ),
+                intervalFactory(
+                    dateFactory('СР', 3, 0, 0),
+                    dateFactory('ЧТ', 7, 0, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 0, 0),
+                    dateFactory('ЧТ', 8, 30, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 31, 0),
+                    dateFactory('ПТ', 8, 30, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ПТ', 8, 30, 0),
+                    dateFactory('СБ', 8, 30, 0),
+                )
+            ]
+            ), [
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0, 0),
+                    dateFactory('ЧТ', 7, 0, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 0, 0),
+                    dateFactory('ЧТ', 8, 30, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 31, 0),
+                    dateFactory('СБ', 8, 30, 0),
+                )
+            ]);
+    });
+
+    it('должен переводить интервалы в минуты', () => {
+        assert.deepStrictEqual(
+            robbery.intervalToMinutes(
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0, 0),
+                    dateFactory('ВТ', 7, 30, 0),
+                )), 30);
+
+        assert.deepStrictEqual(
+            robbery.intervalToMinutes(intervalFactory(
+                dateFactory('ВТ', 7, 0, 0),
+                dateFactory('ВТ', 8, 30, 0),
+            )), 90);
+
+        assert.deepStrictEqual(
+            robbery.intervalToMinutes(
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0, 0),
+                    dateFactory('СР', 8, 30, 0),
+                )), 1530);
+
+        assert.deepStrictEqual(
+            robbery.intervalToMinutes(intervalFactory(
+                dateFactory('ВТ', 7, 0, 0),
+                dateFactory('СР', 0, 0, 0),
+            )), 1020);
+
+        assert.deepStrictEqual(
+            robbery.intervalToMinutes(intervalFactory(
+                dateFactory('ВТ', 7, 0, 0),
+                dateFactory('СР', 1, 30, 0),
+            )), 1110);
+    });
+
+    it('должен извлекать свободное время', () => {
+        assert.deepStrictEqual(
+            robbery.invertIntervals([
+                intervalFactory(
+                    dateFactory('ВТ', 7, 0),
+                    dateFactory('ЧТ', 7, 30),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 0),
+                    dateFactory('ЧТ', 8, 30),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 31),
+                    dateFactory('СБ', 8, 30),
+                )
+            ]), [
+                intervalFactory(
+                    dateFactory('ПН', 0, 0),
+                    dateFactory('ВТ', 7, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 7, 30),
+                    dateFactory('ЧТ', 8, 0),
+                ),
+                intervalFactory(
+                    dateFactory('ЧТ', 8, 30),
+                    dateFactory('ЧТ', 8, 31),
+                ),
+                intervalFactory(
+                    dateFactory('СБ', 8, 30),
+                    dateFactory('ВС', 23, 59),
+                )
+            ]);
+    });
+
     it('должен форматировать существующий момент', () => {
         const moment = getMomentFor(90);
 
@@ -35,6 +245,29 @@ describe('robbery.getAppropriateMoment()', () => {
         assert.strictEqual(
             moment.format('Метим на %DD, старт в %HH:%MM!'),
             'Метим на ВТ, старт в 11:30!'
+        );
+    });
+
+    it('Должен добавлять к дате минуты', () => {
+        assert.deepStrictEqual(
+            robbery.addMinutes(dateFactory('ПН', 0, 0, 0), 30),
+            dateFactory('ПН', 0, 30, 0)
+        );
+        assert.deepStrictEqual(
+            robbery.addMinutes(dateFactory('ПН', 0, 0, 0), 90),
+            dateFactory('ПН', 1, 30, 0)
+        );
+        assert.deepStrictEqual(
+            robbery.addMinutes(dateFactory('ПН', 0, 0, 0), 60 * 24 + 60 + 30),
+            dateFactory('ВТ', 1, 30, 0)
+        );
+        assert.deepStrictEqual(
+            robbery.addMinutes(dateFactory('ПН', 23, 59, 0), 1),
+            dateFactory('ВТ', 0, 0, 0)
+        );
+        assert.deepStrictEqual(
+            robbery.addMinutes(dateFactory('ПН', 23, 59, 0), -1),
+            dateFactory('ПН', 23, 58, 0)
         );
     });
 
@@ -48,29 +281,29 @@ describe('robbery.getAppropriateMoment()', () => {
         );
     });
 
-    if (robbery.isStar) {
-        it('должен перемещаться на более поздний момент [*]', () => {
-            const moment = getMomentFor(90);
+    // if (robbery.isStar) {
+    it('должен перемещаться на более поздний момент [*]', () => {
+        const moment = getMomentFor(90);
 
-            assert.ok(moment.tryLater());
-            assert.strictEqual(moment.format('%DD %HH:%MM'), 'ВТ 16:00');
+        assert.ok(moment.tryLater());
+        assert.strictEqual(moment.format('%DD %HH:%MM'), 'ВТ 16:00');
 
-            assert.ok(moment.tryLater());
-            assert.strictEqual(moment.format('%DD %HH:%MM'), 'ВТ 16:30');
+        assert.ok(moment.tryLater());
+        assert.strictEqual(moment.format('%DD %HH:%MM'), 'ВТ 16:30');
 
-            assert.ok(moment.tryLater());
-            assert.strictEqual(moment.format('%DD %HH:%MM'), 'СР 10:00');
-        });
+        assert.ok(moment.tryLater());
+        assert.strictEqual(moment.format('%DD %HH:%MM'), 'СР 10:00');
+    });
 
-        it('не должен сдвигать момент, если более позднего нет [*]', () => {
-            const moment = getMomentFor(90);
+    it('не должен сдвигать момент, если более позднего нет [*]', () => {
+        const moment = getMomentFor(90);
 
-            assert.ok(moment.tryLater());
-            assert.ok(moment.tryLater());
-            assert.ok(moment.tryLater());
+        assert.ok(moment.tryLater());
+        assert.ok(moment.tryLater());
+        assert.ok(moment.tryLater());
 
-            assert.ok(!moment.tryLater());
-            assert.strictEqual(moment.format('%DD %HH:%MM'), 'СР 10:00');
-        });
-    }
+        assert.ok(!moment.tryLater());
+        assert.strictEqual(moment.format('%DD %HH:%MM'), 'СР 10:00');
+    });
+    // }
 });
