caab0e0a5d51f6ad7ef039cf5088cd40b2cae3c2
Liza Ginzburg <lizagin98@gmail.com>
2018-10-28 16:48:19 +0500

    first without extra

--- robbery.js
+++ robbery.js
@@ -14,8 +14,20 @@ const isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
+
+const MINUTES_IN_HOURS = 60;
+
+const DAYS = {
+    'ПН': 0,
+    'ВТ': 24,
+    'СР': 48
+};
+
 function getAppropriateMoment(schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    const bankTimeZone = getTimezone(workingHours.from);
+    const bankSchedule = getBankSchedule(getBankScheduleInDatestamp(workingHours));
+    const robberSchedule = getRobberSchedule(schedule, bankTimeZone);
+    const timeRangesToRobbery = getTimeToRobbery (robberSchedule, bankSchedule, duration);
 
     return {
 
@@ -24,7 +36,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return timeRangesToRobbery.length > 0;
         },
 
         /**
@@ -34,7 +46,17 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (timeRangesToRobbery.length === 0) {
+                return '';
+            }
+
+            const start = timeRangesToRobbery[0].from;
+            const [day, hours, minutes] = minutesToDatestamp(start);
+
+            return template
+                .replace('%DD', day)
+                .replace('%HH', hours)
+                .replace('%MM', minutes);
         },
 
         /**
@@ -46,6 +68,130 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             return false;
         }
     };
+}
+
+function getTimezone(datestamp) {
+    return parseInt(datestamp[datestamp.length - 1]);
+}
+
+function getDays(datestamp) {
+    return datestamp.substring(0, 2);
+}
+
+function getHours(datestamp) {
+    return parseInt(datestamp.substring(3, 5));
+}
+
+function getMinutes(datestamp) {
+    return parseInt(datestamp.substring(6, 8));
+}
+
+function datestampToMinutes(datestamp) {
+    return (DAYS[getDays(datestamp)] + getHours(datestamp)) *
+     MINUTES_IN_HOURS + getMinutes(datestamp);
+}
+
+function minutesToDatestamp(time) {
+    const minutes = time % 60;
+    const h = (time - minutes) / 60;
+    const hours = h % 24;
+    const days = h - hours;
+    const day = getKeyByValue(DAYS, days);
+    const minutesStr = (minutes < 10)
+        ? '0' + minutes.toString() : minutes;
+
+    return [day, hours, minutesStr];
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function robberDatestampToMinutes(bankTimeZone, robberDatestamp) {
+    const robberTimeZone = getTimezone(robberDatestamp);
+    const diff = bankTimeZone - robberTimeZone;
+
+    return datestampToMinutes(robberDatestamp) + diff * 60;
+}
+
+function getBankScheduleInDatestamp(workingHours) {
+    const schedule = [];
+    Object.keys(DAYS).forEach(day => {
+        const daySchedule = { 'from': day + ' ' + workingHours.from,
+            'to': day + ' ' + workingHours.to };
+        schedule.push(daySchedule);
+    });
+
+    return schedule;
+}
+
+function getBankSchedule(schedule) {
+    schedule.forEach(timeRange => {
+        timeRange.from = datestampToMinutes(timeRange.from);
+        timeRange.to = datestampToMinutes(timeRange.to);
+    });
+
+    return schedule;
+}
+
+function getRobberSchedule(schedule, bankTimeZone) {
+    Object.keys(schedule).forEach(robberSchedule => {
+        schedule[robberSchedule].forEach(timeRange => {
+            timeRange.from = robberDatestampToMinutes(bankTimeZone, timeRange.from);
+            timeRange.to = robberDatestampToMinutes(bankTimeZone, timeRange.to);
+        });
+        getFreeTime(schedule[robberSchedule]);
+    });
+
+    return schedule;
+}
+
+function getFreeTime(robberSchedule) {
+    let start = 0;
+    let middle = 0;
+    let finish = 4320;
+    robberSchedule.forEach(timeRange => {
+        if (start < timeRange.from) {
+            middle = timeRange.to;
+            timeRange.to = timeRange.from;
+            timeRange.from = start;
+        }
+        start = middle;
+    });
+
+    if (start < finish) {
+        robberSchedule.push({ 'from': start, 'to': finish });
+    }
+
+    return robberSchedule;
+}
+
+function isIntersected(left, right) {
+    return !(left.from >= right.to || left.to <= right.from);
+}
+
+function getIntersection(left, right) {
+    const intersected = [];
+    left.forEach(l => {
+        right.forEach(r => {
+            if (isIntersected(l, r)) {
+                intersected.push({
+                    from: Math.max(l.from, r.from),
+                    to: Math.min(l.to, r.to)
+                });
+            }
+        });
+    });
+
+    return intersected;
+}
+
+function getTimeToRobbery(robberSchedule, bankSchedule, duration) {
+    const twoRobbers = getIntersection(robberSchedule.Danny, robberSchedule.Linus);
+    const threeRobbers = getIntersection(twoRobbers, robberSchedule.Rusty);
+    const timeRangeToRobber = getIntersection(threeRobbers, bankSchedule);
+
+    return timeRangeToRobber.filter(interval => interval.to - interval.from >= duration);
 }
 
 module.exports = {
