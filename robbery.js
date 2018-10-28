15dca75c0761e635f8c04ed0d7a2116fcbe9c29c
Andrew <avural-k@mail.ru>
2018-10-28 17:46:26 +0500

    Гомзяков Андрей

--- robbery.js
+++ robbery.js
@@ -6,6 +6,81 @@
  */
 const isStar = true;
 
+let timeLine = [];
+
+function ConvertDayToMinuts(day) {
+    if (day === "") {
+
+        return 24 * 60;
+    }
+    if (day === "") {
+
+        return 48 * 60;
+    }
+    if (day === "") {
+
+        return 72 * 60;
+    }
+}
+
+function ConvertminutsToFormat(minuts, bankTimeZone) {
+    minuts += bankTimeZone * 60;
+    let day = Math.trunc(minuts / 24 * 60);
+    minuts -= day * 24 * 60;
+    let hours = Math.trunc(minuts / 60);
+    minuts -= hours * 60;
+    let time = {
+        hours : hours,
+        minuts : minuts,
+    };
+    if (day === 1) {
+        time.day = 'ПН';
+    }
+    if (day === 2) {
+        time.day = 'ВТ';
+    }
+    if (day === 3) {
+        time.day = 'СР';
+    }
+
+    return time;
+}
+
+function ConvertToMinuts(timeString) {
+    let minuts = 0;
+    minuts += ConvertDayToMinuts(timeString.slice(0, 2)); 
+    let hours = parseInt(timeString.match("(\\d\\d):(\\d\\d)")[1]);
+    minuts += parseInt(timeString.match("(\\d\\d):(\\d\\d)")[2]);
+    let timeZone = parseInt(timeString.match("\+(\\d)")[1]);
+    minuts += (hours - timeZone) * 60;
+
+    return minuts;
+}
+
+function AddWorkingHoursToTimeLine(workingHours) {
+    timeLine.push({ first : ConvertToMinuts("ПН " + workingHours.from), second : 1 });
+    timeLine.push({ first : ConvertToMinuts("ПН " + workingHours.to), second : -1 });
+    timeLine.push({ first : ConvertToMinuts("ВТ " + workingHours.from), second : 1 });
+    timeLine.push({ first : ConvertToMinuts("ВТ " + workingHours.to), second : -1 });
+    timeLine.push({ first : ConvertToMinuts("СР " + workingHours.from), second : 1 });
+    timeLine.push({ first : ConvertToMinuts("СР " + workingHours.to), second : -1 });
+}
+
+function scanLine(duration, lastAppropriateMoment) {
+    let notBusy = 0;
+    for (let i = 0; i < timeLine.length - 1; i++) {
+        notBusy += timeLine[i].second;
+        if (notBusy === 4 &&
+            timeLine[i + 1].first - timeLine[i].first >= duration &&
+            timeLine[i].first > lastAppropriateMoment) {
+
+            return timeLine[i].first;
+        }
+
+    }
+    return -1;
+}
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -14,8 +89,21 @@ const isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
+
 function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
+    let bankTimeZone = parseInt(workingHours.from.match('\+(\\d)')[1]);
+    let keys = Object.keys(schedule);
+    for (let i = 0; i < keys.length; i++) {
+        let roberSchedule = schedule[keys[i]];
+        for (let j = 0; j < roberSchedule; j++) {
+            timeLine.push({ first : ConvertToMinuts(roberSchedule[j].from), second : 1 });
+            timeLine.push({ first : ConvertToMinuts(roberSchedule[j].to), second : -1 })
+        }
+    }
+    AddWorkingHoursToTimeLine(workingHours);
+    timeLine.sort((a, b) => a.first - b.first);
+    let approproateMoment = scanLine(duration, -1);
 
     return {
 
@@ -24,7 +112,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return approproateMoment !== -1;
         },
 
         /**
@@ -34,7 +122,16 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (approproateMoment !== -1) {
+                let time = ConvertminutsToFormat(approproateMoment, bankTimeZone);
+                template.replace('%HH', time.hours.toString());
+                template.replace('%MM', time.minuts.toString());
+                template.replace('%DD', time.day);
+
+                return template;
+            } else {
+                return '';
+            }
         },
 
         /**
@@ -43,7 +140,14 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            let newApproproateMoment = scanLine(duration, approproateMoment);
+            if (newApproproateMoment !== -1) {
+                approproateMoment = newApproproateMoment;
+
+                return true;
+            } else {
+                return false;
+            }
         }
     };
 }
