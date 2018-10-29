dbb073da9698ce3c3011701be8bf06acb88f449c
frosthamster <frosthamster@gmail.com>
2018-10-30 04:35:22 +0500

    _/¯

--- robbery.js
+++ robbery.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-const isStar = true;
+const isStar = false;
 
 const TIME_RE = /^(\d{2}):(\d{2})\+(\d{1,2})$/;
 const DATE_TIME_RE = /^(ПН|ВТ|СР) (\d{2}):(\d{2})\+(\d{1,2})$/;
