40bedc917c4c4955b5525bb5e82666632f9d7f20
phi191 <phi191@yandex-team.ru>
2018-10-29 20:47:51 +0500

    Check without star

--- robbery.js
+++ robbery.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-const isStar = true;
+const isStar = false;
 
 const DAYS_OF_WEEK = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 const WORKING_DAYS = ['ПН', 'ВТ', 'СР'];
