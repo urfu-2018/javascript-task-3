294e461f3e831fd974440251cc5df7ec7f028fbc
Boyarnikov Semyon <iboyarnikoff@gmail.com>
2018-10-30 01:24:02 +0500

    Адовая жесть ещё не решённая

--- robbery.js
+++ robbery.js
@@ -17,6 +17,97 @@ const isStar = true;
 function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
 
+    function timeInMunutes(time) {
+        const [hours, minutes] = time.split(':');
+        hours = parseInt(hours);
+        minutes = parseInt(minutes);
+
+        return hours * 60 + minutes;
+    }
+
+    function calculateDifferenceInTimeZones(personTimeZone, bankTimeZone) {
+        return (bankTimeZone - personTimeZone) * 60;
+    }
+
+    function hoursToMinutes(timeAndTimeZone, bankTimeZone) {
+        const [time, timeZone] = timeAndTimeZone.split('+');
+        timeZone = parseInt(timeZone);
+        let formattedToMinutesTime =
+            timeInMunutes(time) + bankTimeZone
+                ? calculateDifferenceInTimeZones(timeZone, bankTimeZone)
+                : 0;
+
+        return formattedToMinutesTime;
+    }
+
+    function formatSchedule(bankTimeZone) {
+        Object.keys(schedule).forEach(person => {
+            schedule[person].forEach(({ from, to }) => {
+                const [dayFrom, timeFrom] = from.split(' ');
+                const [dayTo, timeTo] = to.split(' ');
+
+                return {
+                    from: [dayFrom, hoursToMinutes(timeFrom, bankTimeZone)],
+                    to: [dayTo, hoursToMinutes(timeTo, bankTimeZone)]
+                };
+            });
+        });
+    }
+
+    function getBankTimeZone() {
+        return workingHours.from.split('+')[1];
+    }
+
+    function formatBankTime() {
+        workingHours = {
+            from: hoursToMinutes(workingHours.from),
+            to: hoursToMinutes(workingHours.to)
+        };
+    }
+
+    function createDaysTimeRange() {
+        const ob = { ПН: [], ВТ: [], СР: [] };
+
+        Object.keys(schedule).forEach(person => {
+            schedule[person].forEach(({ from, to }) => {
+                if (from[0] === to[0]) {
+                    ob[from[0]].push([from[1], to[1]]);
+                } else {
+                    ob[from[0]].push([from[1], workingHours[to]]);
+                    ob[to[0]].push(workingHours[from], to[0]);
+                }
+            });
+        });
+
+        return ob;
+    }
+
+    function compareRanges(supposedRange, dayRange) {
+        return (
+            (supposedRange[0] < dayRange[0] && supposedRange[1] < dayRange[1]) ||
+            (supposedRange[0] > dayRange[0] && supposedRange[1] > dayRange[1])
+        );
+    }
+
+    function findTimeRange(duration) {
+        const bankTimeZone = getBankTimeZone();
+
+        formatBankTime();
+        formatSchedule(bankTimeZone);
+
+        const invalidTimeRanges = createDaysTimeRange();
+
+        for (let i = workingHours.from; i < workingHours.to - duration; i++) {
+            Object.keys(invalidTimeRanges).forEach(day => {
+                invalidTimeRanges[day].forEach(range => {
+                    if (!compareRanges([i, i + duration], range)) {
+                        return false;
+                    }
+                });
+            });
+        }
+    }
+
     return {
 
         /**
