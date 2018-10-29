7e4c0d1ceef16cf72fb47ee5d971ba579ac0c044
kartsev <kartsev@skbkontur.ru>
2018-10-29 19:17:10 +0500

    resolve task

--- robbery.js
+++ robbery.js
@@ -5,44 +5,12 @@
  * Реализовано оба метода и tryLater
  */
 const isStar = true;
+let appropriateMoments;
+let bankWorkingHours;
+const newSchedule = { 'ПН': [], 'ВТ': [], 'СР': [] };
 
-function groupBy(xs, key) {
-    return xs.reduce(function (rv, x) {
-        (rv[x[key]] = rv[x[key]] || []).push(x);
-
-        return rv;
-    }, {});
-}
-
-function getWeekNumber(day) {
-    switch (day) {
-        case 'ПН':
-            return 0;
-        case 'ВТ':
-            return 1;
-        case 'СР':
-            return 2;
-        case 'ЧТ':
-            return 3;
-        case 'ПТ':
-            return 4;
-        case 'СБ':
-            return 5;
-        case 'ВС':
-            return 6;
-    }
-}
-
-/**
- * @param {Object} schedule – Расписание Банды
- * @param {Number} duration - Время на ограбление в минутах
- * @param {Object} workingHours – Время работы банка
- * @param {String} workingHours.from – Время открытия, например, "10:00+5"
- * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
- * @returns {Object}
- */
 function parseToUtc(inputStr) {
-    let day = getWeekNumber(inputStr.slice(0, 2));
+    let day = inputStr.slice(0, 2);
 
     const localHour = parseInt(inputStr.slice(3, 5));
     const minutes = parseInt(inputStr.slice(6, 8));
@@ -54,45 +22,77 @@ function parseToUtc(inputStr) {
         day = (7 + day) % day;
     }
 
-    return { day: day, hour: fromUtcHour, minutes: minutes };
+    return { day: day, minutes: fromUtcHour * 60 + minutes };
 }
+
+function getTimeRanges(scheduleInDay, dayName) {
+    const result = [];
+    let leftBorder = 0;
+    for (let i = 0; i < scheduleInDay.length; i++) {
+        if (scheduleInDay[i].fromInUtc.minutes >= leftBorder) {
+            result.push({ day: dayName, from: leftBorder, to: scheduleInDay[i].fromInUtc.minutes });
+        }
+        leftBorder = scheduleInDay[i].toInUtc.minutes;
+    }
+
+    const minutesInDay = 23 * 60 + 59;
+    result.push({ day: dayName, from: leftBorder, to: minutesInDay });
+
+    return result;
+}
+
+function getWorkingHours(workingHours) {
+    const fromHour = parseInt(workingHours.from.slice(0, 2));
+    const toHour = parseInt(workingHours.to.slice(0, 2));
+    const shift = parseInt(workingHours.from.slice(6));
+
+    return {
+        shift: shift,
+        fromInUtc: {
+            minutes: (24 + fromHour - shift) % 24 * 60 + parseInt(workingHours.from.slice(3, 5))
+        },
+        toInUtc: {
+            minutes: (24 + toHour - shift) % 24 * 60 + parseInt(workingHours.to.slice(3, 5))
+        }
+    };
+}
+
+function fillSchedule(robberSchedule, robberName) {
+    const fromInUtc = parseToUtc(robberSchedule.from);
+    const toInUtc = parseToUtc(robberSchedule.to);
+    if (fromInUtc.day === toInUtc.day) {
+        newSchedule[fromInUtc.day].push(
+            { name: robberName, fromInUtc: fromInUtc, toInUtc: toInUtc });
+
+        return;
+    }
+    newSchedule[fromInUtc.day].push(
+        {
+            name: robberName,
+            fromInUtc: fromInUtc,
+            toInUtc: { day: fromInUtc.day, minutes: 23 * 60 + 59 }
+        });
+    newSchedule[toInUtc.day].push({
+        name: robberName,
+        fromInUtc: { day: toInUtc.day, minutes: 0 },
+        toInUtc: toInUtc
+    });
+
+    return newSchedule;
+}
+
+/**
+ * @param {Object} schedule – Расписание Банды
+ * @param {Number} duration - Время на ограбление в минутах
+ * @param {Object} workingHours – Время работы банка
+ * @param {String} workingHours.from – Время открытия, например, "10:00+5"
+ * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
+ * @returns {Object}
+ */
 
 function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
-    workingHours = {
-        shift: parseInt(workingHours.from.slice(6)),
-        from: {
-            hour: parseInt(workingHours.from.slice(0, 2)),
-            minutes: parseInt(workingHours.from.slice(3, 5))
-        },
-        to: {
-            hour: parseInt(workingHours.to.slice(0, 2)),
-            minutes: parseInt(workingHours.to.slice(3, 5))
-        }
-    };
-    let newSchedule = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [] };
-
-    function fillSchedule(robberSchedule, robberName) {
-        const fromInUtc = parseToUtc(robberSchedule.from);
-        const toInUtc = parseToUtc(robberSchedule.to);
-        if (fromInUtc.day === toInUtc.day) {
-            newSchedule[fromInUtc.day].push(
-                { name: robberName, fromInUtc: fromInUtc, toInYtc: toInUtc });
-
-            return;
-        }
-        newSchedule[fromInUtc.day].push(
-            {
-                name: robberName,
-                fromInUtc: fromInUtc,
-                toInYtc: { day: fromInUtc.day, hour: 23, minutes: 59 }
-            });
-        newSchedule[toInUtc.day].push({
-            name: robberName,
-            fromInUtc: { day: toInUtc.day, hour: 0, minutes: 0 },
-            toInYtc: toInUtc
-        });
-    }
+    bankWorkingHours = getWorkingHours(workingHours);
 
     for (let propName in schedule) {
         if (!Array.isArray(schedule[propName])) {
@@ -103,23 +103,25 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             fillSchedule(robberSchedule[i], propName);
         }
     }
-    const anies = Object.keys(newSchedule).filter(x=>newSchedule[x].length > 0)
-        .map(x=>newSchedule[x]
-            .sort((y, z) =>
-                y.fromInUtc.day - z.fromInUtc.day ||
-            y.fromInUtc.hour - z.fromInUtc.hour || y.fromInUtc.minutes - z.fromInUtc.minutes)
-            .reduce((y, z)=> {
-                return { fromInUtc: z.fromInUtc, toInYtc: y.toInYtc };
-            }));
+    appropriateMoments = Object.keys(newSchedule)
+        .map(x => newSchedule[x]
+            .sort((y, z) => y.fromInUtc.minutes - z.fromInUtc.minutes))
+        .map(x => {
+            return getTimeRanges(x, x[0].fromInUtc.day)
+                .filter(a => a.from !== a.to)
+                .map(a => {
+                    const rightBorder = Math.min(bankWorkingHours.toInUtc.minutes, a.to);
+                    const leftBorder = Math.max(bankWorkingHours.fromInUtc.minutes, a.from);
 
-    // const groupBy1 = groupBy(schedule, 'day');
-    // const filter = schedule
-    //     .map(x => x.filter(
-    //         y => y.from.hour >= workingHours.from.hour &&
-    //             y.from.minutes >= workingHours.from.minutes &&
-    //             y.to.hour <= workingHours.to.hour &&
-    //             y.to.minutes >= workingHours.to.minutes)
-    //     );
+                    return { day: a.day, from: leftBorder, to: rightBorder };
+                })
+                .filter(a => a.to - a.from >= duration);
+        })
+        .filter(x => x.length !== 0);
+
+    if (appropriateMoments.length !== 0) {
+        appropriateMoments = appropriateMoments.reduce((x, y) => x.concat(y));
+    }
 
     return {
 
@@ -128,7 +130,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return appropriateMoments.length !== 0;
         },
 
         /**
@@ -138,7 +140,15 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (appropriateMoments.length === 0) {
+                return '';
+            }
+
+            return template.replace(
+                '%HH', Math.floor(appropriateMoments[0].from / 60) + bankWorkingHours.shift)
+                .replace('%MM', (appropriateMoments[0].from % 60).toString()
+                    .padStart(2, '0'))
+                .replace('%DD', appropriateMoments[0].day);
         },
 
         /**
@@ -147,7 +157,17 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            if (appropriateMoments.length <= 1) {
+                return false;
+            }
+            if (appropriateMoments[0].from + 30 + duration <= appropriateMoments[0].to) {
+                appropriateMoments[0].from = appropriateMoments[0].from + 30;
+
+                return true;
+            }
+            appropriateMoments.splice(0, 1);
+
+            return true;
         }
     };
 }
