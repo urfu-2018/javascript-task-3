d8b6823f7531fb0fe11e230cfb9c317c30ad13c1
hukopo <hukopo@yandex.ru>
2018-10-30 08:34:10 +0500

    init

--- robbery.js
+++ robbery.js
@@ -16,7 +16,8 @@ const isStar = true;
  */
 function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
-
+    getNewScheduleFormat(schedule)
+    console.info(days);
     return {
 
         /**
@@ -48,8 +49,59 @@ function getAppropriateMoment(schedule, duration, workingHours) {
     };
 }
 
+function DayToMinutes(day) {
+    if (day === 'ПН') {
+        return 0;
+    } else if (day === 'ВТ') {
+        return 24 * 60;
+    } else if (day === 'СР') {
+        return 24 * 2 * 60;
+    } else
+        return -1;
+}
+
+function getNewScheduleFormat(schedule) {
+    const newSchedule = { Danny: [], Rusty: [], Linus: [] }
+    schedule.Danny.map(r => getNewScheduleRowFormat(r))
+}
+
+function getNewScheduleRowFormat(r) {
+    const newFrom = DayToMinutes(r.from.substring(0, 2)) + parseTimeToMinutes(r.from.substring(3));
+    const newTo = DayToMinutes(r.to.substring(0, 2)) + parseTimeToMinutes(r.to.substring(3));
+    if (newTo - newFrom < duration) {
+        return null;
+    }
+    else {
+        return { from: newFrom, to: newTo }
+    }
+}
+
+const days = ['ПН', 'ВТ', 'СР'];
+function mmayb1eDays(scheduleBoy) {
+    days.forEach(e => {
+        if (!maybeDay(scheduleBoy, e)) {
+            days.splice(days.indexOf(e), 1)
+        }
+    });
+}
+
+function maybeDay(schedule, day) {
+    let haveTimeIsCurrentDay = false;
+    schedule.forEach(element => {
+        if (element.from.indexOf(day) !== -1) {
+            haveTimeIsCurrentDay = true;
+        }
+    });
+    return haveTimeIsCurrentDay;
+}
+
+function parseTimeToMinutes(time) {
+    return parseInt(time.substring(0, 2)) * 60 + parseInt(time.substring(3, 5)) + parseInt(time.substring(6, 7)) * 60;
+}
+
 module.exports = {
     getAppropriateMoment,
 
+    parseTimeToMinutes,
     isStar
 };
