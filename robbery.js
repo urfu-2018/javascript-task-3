d7d8427042b9ba34eda95019ee0a55ae3cc3402c
Boyarnikov Semyon <iboyarnikoff@gmail.com>
2018-10-30 20:14:33 +0500

    ))))

--- robbery.js
+++ robbery.js
@@ -108,13 +108,13 @@ function findTimeRange(daysRanges, duration, formattedWorkingHours) {
 
                 if (counter === daysRanges[day].length) {
                     variants[day].push(i);
-                    i += 29;
                 }
             } else {
                 variants[day].push(i);
-                i += 29;
             }
         });
+
+        i += 29;
     }
 
     return variants;
