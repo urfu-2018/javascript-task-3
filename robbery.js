727264ece39d3a0a4385f4fae55a6dae3e22d7b5
Unknown <valera9719@gmail.com>
2018-10-30 17:16:47 +0500

    commit

--- robbery.js
+++ robbery.js
@@ -1,10 +1,80 @@
+/*eslint-disable */
+
 'use strict';
 
 /**
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-const isStar = true;
+exports.isStar = true;
+
+const days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+function convertTime(time, newZone) {
+
+    return toMinutes(time) + (newZone - Number(time.split('+')[1])) * 60;
+}
+
+function toMinutes(time) {
+    return days.indexOf(time.slice(0, 2)) * 1440 +
+        Number(time.slice(3, 5)) * 60 +
+        Number(time.slice(6, 8));
+}
+
+function toDate(minutes) {
+    let day = Math.floor(minutes / 1440);
+    let hours = Math.floor((minutes - day * 1440) / 60);
+    minutes -= day * 1440 + hours * 60;
+    let newMinutes = `0${minutes}`.slice(-2);
+    let newHours = `0${hours}`.slice(-2);
+
+    return { day: days[day], hours: newHours, minutes: newMinutes };
+}
+
+function without(range1, range2) {
+    if (range1.to <= range2.from || range2.to <= range1.from) {
+        return [range1];
+    }
+    if (range2.from <= range1.from && range1.to <= range2.to) {
+        return [null];
+    }
+    if (range1.from <= range2.from && range2.to <= range1.to) {
+        return [
+            { from: range1.from, to: range2.from },
+            { from: range2.to, to: range1.to }
+        ];
+    }
+    if (range1.from <= range2.from) {
+        return [{ from: range1.from, to: range2.from }];
+    }
+
+    return [{ from: range2.to, to: range1.to }];
+}
+
+function replace(element, array, position) {
+    if (element) {
+        array[position] = element;
+
+        return 1;
+    }
+    array.splice(position, 1);
+
+    return 0;
+}
+
+function getMomentForPair(goodTime, badTime) {
+    for (let bad of badTime) {
+        let i = 0;
+        while (i < goodTime.length) {
+            let listOfGood = without(goodTime[i], bad);
+            let j = replace(listOfGood[0], goodTime, i);
+            i = i + j;
+            replace(listOfGood[1], goodTime, goodTime.length);
+        }
+    }
+
+    return goodTime;
+}
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -14,27 +84,54 @@ const isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
-function getAppropriateMoment(schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+exports.getAppropriateMoment = function (schedule, duration, workingHours) {
+    let listOfTimes = [];
+    let bankZone = Number(workingHours.to.split('+')[1]);
+    Object.keys(schedule).forEach(guy => {
+        listOfTimes.push(schedule[guy].map(time => ({
+            from: convertTime(time.from, bankZone),
+            to: convertTime(time.to, bankZone) })
+        ));
+    });
+    let goodTime = listOfTimes.reduce(getMomentForPair, [
+        { from: toMinutes('ПН ' + workingHours.from), to: toMinutes('ПН ' + workingHours.to) },
+        { from: toMinutes('ВТ ' + workingHours.from), to: toMinutes('ВТ ' + workingHours.to) },
+        { from: toMinutes('СР ' + workingHours.from), to: toMinutes('СР ' + workingHours.to) }
+    ]);
+
+    let appropriateTime = goodTime.filter(time => time.to - time.from >= duration)
+        .sort((obj1, obj2) => obj1.from - obj2.from);
 
     return {
+        allMoments: appropriateTime,
+        momentNumber: 0,
+        duration: duration,
 
         /**
          * Найдено ли время
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return Boolean(this.allMoments[this.momentNumber]);
         },
 
         /**
          * Возвращает отформатированную строку с часами для ограбления
-         * Например, "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
+         * Например,
+         *   "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
          * @param {String} template
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            let time = this.allMoments[this.momentNumber];
+            if (!time) {
+                return '';
+            }
+            time = toDate(time.from);
+
+            return template.replace('%DD', time.day)
+                .replace('%HH', time.hours)
+                .replace('%MM', time.minutes);
         },
 
         /**
@@ -43,13 +140,26 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+            let time = this.allMoments[this.momentNumber];
+            if (!time) {
+                return false;
+            }
+            if (time.to - time.from >= this.duration + 30) {
+                this.allMoments[this.momentNumber] = {
+                    from: time.from + 30,
+                    to: time.to
+                };
+
+                return true;
+            }
+            let possibleNext = this.allMoments[this.momentNumber + 1];
+            if (possibleNext && possibleNext.from - time.to >= 30) {
+                this.momentNumber++;
+
+                return true;
+            }
+
             return false;
         }
     };
-}
-
-module.exports = {
-    getAppropriateMoment,
-
-    isStar
 };
