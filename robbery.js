fb7df7f6614ea8426100626af670854206a66173
creewick <ilia_Y@mail.ru>
2018-10-30 15:07:28 +0500

    exception fix

--- robbery.js
+++ robbery.js
@@ -128,6 +128,9 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+            if (!this.exists()) {
+                return false;
+            }
             if (this.currentTime + 30 + duration <= this.intervals[this.currentIndex].to) {
                 this.currentTime += 30;
 
@@ -149,3 +152,25 @@ module.exports = {
     getAppropriateMoment,
     isStar
 };
+
+let a = getAppropriateMoment(
+    {
+        Danny: [
+            { from: 'ПН 12:00+5', to: 'ПН 17:00+5' },
+            { from: 'ВТ 13:00+5', to: 'ВТ 16:00+5' }
+        ],
+        Rusty: [
+            { from: 'ПН 11:30+5', to: 'ПН 16:30+5' },
+            { from: 'ВТ 13:00+5', to: 'ВТ 16:00+5' }
+        ],
+        Linus: [
+            { from: 'ПН 09:00+3', to: 'ПН 14:00+3' },
+            { from: 'ПН 21:00+3', to: 'ВТ 09:30+3' },
+            { from: 'СР 09:30+3', to: 'СР 15:00+3' }
+        ]
+    },
+    250,
+    { from: '10:00+5', to: '18:00+5' }
+);
+
+let b = a.tryLater();
