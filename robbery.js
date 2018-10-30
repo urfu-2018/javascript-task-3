974b49a0e26f3b8869f3a56cfdd7b8820c960bea
epeshk <peshkov@skbkontur.ru>
2018-10-30 13:36:55 +0500

    try

--- robbery.js
+++ robbery.js
@@ -6,6 +6,56 @@
  */
 const isStar = true;
 
+const MINUTES_IN_HOUR = 60;
+const HOURS_IN_DAY = 24;
+const MINUTES_IN_DAY = HOURS_IN_DAY * MINUTES_IN_HOUR;
+
+const days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+class Time {
+    constructor(day, hour, minute, offset) {
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.offset = offset;
+    }
+
+    getTimestamp(timezone) {
+        return this.day * MINUTES_IN_DAY +
+        this.hour * MINUTES_IN_HOUR +
+        this.minute +
+            (timezone - this.offset) * MINUTES_IN_HOUR;
+    }
+
+    getLocalTimeOfDay() {
+        return this.hour * MINUTES_IN_HOUR + this.minute;
+    }
+
+    static parse(s) {
+        let parts = s.split(' ');
+        let day = 0;
+        let timeStr;
+        if (parts.length > 1) {
+            day = getDayNumber(parts[0]);
+            timeStr = parts[1];
+        } else {
+            timeStr = parts[0];
+        }
+
+        return Time.parseTime(day, timeStr);
+    }
+
+    static parseTime(day, s) {
+        const match = s.match(/^(\d+):(\d+)\+(\d+)$/);
+
+        let hour = Number(match[1]);
+        let minute = Number(match[2]);
+        let offset = Number(match[3]);
+
+        return new Time(day, hour, minute, offset);
+    }
+}
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -17,16 +67,22 @@ const isStar = true;
 function getAppropriateMoment(schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
 
-    let isSuitable = buildSchedule(schedule, workingHours);
-    
+    let bankTimeZone = Number(workingHours.from.split('+')[1]);
+
+    let isSuitable = buildSchedule(schedule, workingHours, bankTimeZone);
+
+    let endTime = getDayNumber('ЧТ') * MINUTES_IN_DAY;
+
+    let appropriateMomemts = getAppropriateMinutes(isSuitable, duration, endTime);
+
     return {
+        index: 0,
+
         /**
          * Найдено ли время
          * @returns {Boolean}
          */
-        exists: function () {
-            return false;
-        },
+        exists: () => appropriateMomemts.length > 0,
 
         /**
          * Возвращает отформатированную строку с часами для ограбления
@@ -35,7 +91,19 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (!this.exists()) {
+                return '';
+            }
+            let moment = appropriateMomemts[this.index];
+            let day = Math.floor(moment / MINUTES_IN_DAY);
+            moment %= MINUTES_IN_DAY;
+            let hour = Math.floor(moment / MINUTES_IN_HOUR);
+            moment %= MINUTES_IN_HOUR;
+
+            return template
+                .replace(/%HH/, hour.toString().padStart(2, '0'))
+                .replace(/%MM/, moment.toString().padStart(2, '0'))
+                .replace(/%DD/, days[day]);
         },
 
         /**
@@ -44,32 +112,58 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            if (this.index + 1 >= appropriateMomemts.length) {
+                return false;
+            }
+            ++this.index;
+
+            return true;
         }
     };
 }
 
-function getTimeAfter(isSuitable, minute) {
-    for () 
+function getAppropriateMinutes(isSuitable, duration, endTime) {
+    let freeMinutes = 0;
+    let appropriateMomemts = [];
+
+    for (let i = 0; i < endTime; ++i) {
+        if (!isSuitable(i)) {
+            freeMinutes = 0;
+            continue;
+        }
+
+        if (++freeMinutes < duration - 1) {
+            continue;
+        }
+
+        let previousTime = appropriateMomemts[appropriateMomemts.length - 1];
+        if (previousTime !== undefined && (i - duration + 1) - previousTime < 30) {
+            continue;
+        }
+
+        appropriateMomemts.push(i - duration + 1);
+    }
+
+    return appropriateMomemts;
 }
 
-function buildSchedule(rawSchedule, workingHours) {
-    const busyTimes = enumerateTimes(rawSchedule)
+function buildSchedule(rawSchedule, workingHours, bankTimeZone) {
+    const busyTimes = Array.from(enumerateTimes(rawSchedule))
         .map(x => ({
-            from: convertStringWithDayToMinutes(x.from),
-            to: convertStringWithDayToMinutes(x.to)
+            from: Time.parse(x.from).getTimestamp(bankTimeZone),
+            to: Time.parse(x.to).getTimestamp(bankTimeZone)
         }));
 
-    let openTime = convertStringToMinutes(workingHours.from);
-    let closeTime = convertStringToMinutes(workingHours.to);
+    let openTime = Time.parse(workingHours.from).getLocalTimeOfDay();
+    let closeTime = Time.parse(workingHours.to).getLocalTimeOfDay();
 
     return (time) => {
-        let timeOfDay = time % (24 * 60);
-        if (openTime > timeOfDay || closeTime < timeOfDay) {
+        let timeOfDay = time % MINUTES_IN_DAY;
+        if (!(openTime < timeOfDay && timeOfDay < closeTime)) {
             return false;
         }
 
-        return busyTimes.eachTest(x => !(x.from <= time && time <= x.to));
+        return busyTimes.every(x => !(x.from <= time && time <= x.to));
     };
 }
 
@@ -81,24 +175,8 @@ function *enumerateTimes(schedule) {
     }
 }
 
-function convertStringWithDayToMinutes(s) {
-    let parts = s.split(' ');
-
-    return getDayNumber(s[0]) * 24 * 60 + convertStringToMinutes(s[1]);
-}
-
-function convertStringToMinutes(s) {
-    const match = s.match(/^(\d+):(\d+)\+(\d+)$/);
-
-    const hour = Number(match[1]);
-    const minute = Number(match[2]);
-    const offset = Number(match[3]);
-
-    return (hour + offset) * 60 + minute;
-}
-
 function getDayNumber(day) {
-    return ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'].indexOf(day);
+    return days.indexOf(day);
 }
 
 module.exports = {
