7daa0dfa6e5df81a16b83d1d186c70913c1ec95b
Монин Константин <33116124+Kuvadorian@users.noreply.github.com>
2018-10-30 14:07:01 +0500

    Попробую допы

--- robbery.js
+++ robbery.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-const isStar = false;
+const isStar = true;
 
 const weekDays = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 const minutesInHour = 60;
