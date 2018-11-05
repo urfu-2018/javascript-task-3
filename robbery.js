ed46b29cd38de2e6bd28532c9af887d9960b4143
Akishkina Veronika <nika_akishkina@mail.ru>
2018-11-05 18:47:11 +0500

    throw, если не удовлетворяет условию

--- robbery.js
+++ robbery.js
@@ -21,20 +21,26 @@ function converting(timeStartOrEnd) {
         .set('ВС', 7);
     date.setUTCDate(daysToConvert.get(timeStartOrEnd.substr(0, 2)));
     let timezone = timeStartOrEnd.substr(8, timeStartOrEnd.length - 8);
-    if (timezone[0] === '-' || isNaN(Number(timezone)) === NaN) {
+    timezone = check(timezone);
+    date.setUTCHours(timeStartOrEnd.substr(3, 2));
+    date.setUTCMinutes(timeStartOrEnd.substr(6, 2));
+    date.setUTCSeconds(0);
+    date.setUTCMilliseconds(0);
+    date = date.valueOf() - Number(timezone) * 3600000;
+
+    return date;
+}
+
+function check(timezone) {
+    if (timezone[0] === '-' || Number.isNaN(Number(timezone))) {
         throw new TypeError();
     }
     timezone = Number(timezone);
     if (!Number.isInteger(timezone)) {
         throw new TypeError();
     }
-    date.setUTCHours(timeStartOrEnd.substr(3, 2));
-    date.setUTCMinutes(timeStartOrEnd.substr(6, 2));
-    date.setUTCSeconds(0);
-    date.setUTCMilliseconds(0);
-    date = date.valueOf() - timezone * 3600000;
 
-    return date;
+    return timezone;
 }
 
 function conversionToSpareTime(schedule) {
