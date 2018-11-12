12b515493013459fa58484cb705eee0b2e71bb39
creewick <ilia_Y@mail.ru>
2018-11-12 12:31:22 +0500

    Константы, concat(...array)

--- robbery.js
+++ robbery.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const isStar = true;
-const timeRegex = /([А-Я]{2})? ?(\d{2}):(\d{2})\+(\d)/;
-let days = { 'ВТ': 1, 'СР': 2, 0: 'ПН', 1: 'ВТ', 2: 'СР' };
+const TimeRegex = /([А-Я]{2})? ?(\d{2}):(\d{2})\+(\d)/;
+const Days = { 'ВТ': 1, 'СР': 2, 0: 'ПН', 1: 'ВТ', 2: 'СР' };
+const MinutesInDay = 24 * 60;
+const HoursInDay = 24;
+const MinutesInHour = 60;
+const MaxMinutesValue = 3 * MinutesInDay - 1;
+const SkipMinutes = 30;
 
-let reduceDim = array => [].concat.apply([], array);
-
-let toOneDimArray = threeDimArray => reduceDim(reduceDim(threeDimArray));
+let toOneDimArray = threeDimArray => [].concat(...[].concat(...threeDimArray));
 
 let getPoint = (type, time, timezone) => ({ type: type, time: getMinutes(time, timezone) });
 
@@ -30,9 +33,9 @@ function getFreeIntervals(points) {
 
     points.forEach(p => {
         if (!busy) {
-            if (p.time % (24 * 60) < start % (24 * 60)) {
-                intervals.push({ from: start, to: p.time - p.time % (24 * 60) - 1 });
-                intervals.push({ from: p.time - p.time % (24 * 60), to: p.time });
+            if (p.time % (MinutesInDay) < start % (MinutesInDay)) {
+                intervals.push({ from: start, to: p.time - p.time % (MinutesInDay) - 1 });
+                intervals.push({ from: p.time - p.time % (MinutesInDay), to: p.time });
             } else {
                 intervals.push({ from: start, to: p.time });
             }
@@ -40,25 +43,24 @@ function getFreeIntervals(points) {
         busy += p.type === 'from' ? 1 : -1;
         start = p.time;
     });
-    intervals.push({ from: start, to: 3 * 24 * 60 - 1 });
+    intervals.push({ from: start, to: MaxMinutesValue });
 
     return intervals;
 }
 
-let getDay = (minutes) => Math.floor(minutes / (24 * 60));
-let getHour = (minutes) => Math.floor(minutes / 60) % 24;
-let getMinute = (minutes) => minutes % 60;
+let getDay = (minutes) => Math.floor(minutes / (MinutesInDay));
+let getHour = (minutes) => Math.floor(minutes / MinutesInHour) % HoursInDay;
+let getMinute = (minutes) => minutes % MinutesInHour;
 let twoDigit = (int) => int > 9 ? int : '0' + int;
 
 function getAppropriateIntervals(freeIntervals, bankInterval, duration) {
-    let day = 24 * 60;
     let intervals = [];
 
     freeIntervals.forEach(interval => {
-        let start = bankInterval.from <= interval.from % day
-            ? interval.from : getDay(interval.from) * day + bankInterval.from;
-        let end = bankInterval.to >= interval.to % day
-            ? interval.to : getDay(interval.to) * day + bankInterval.to;
+        let start = bankInterval.from <= interval.from % MinutesInDay
+            ? interval.from : getDay(interval.from) * MinutesInDay + bankInterval.from;
+        let end = bankInterval.to >= interval.to % MinutesInDay
+            ? interval.to : getDay(interval.to) * MinutesInDay + bankInterval.to;
         if (end - start >= duration) {
             intervals.push({
                 from: start,
@@ -71,13 +73,13 @@ function getAppropriateIntervals(freeIntervals, bankInterval, duration) {
 }
 
 function getMinutes(time, timezone) {
-    let match = time.match(timeRegex);
-    let day = days[match[1]] ? days[match[1]] : 0;
+    let match = time.match(TimeRegex);
+    let day = Days[match[1]] ? Days[match[1]] : 0;
     let hours = parseInt(match[2]);
     let minutes = parseInt(match[3]);
     let localzone = parseInt(match[4]);
 
-    return day * 24 * 60 + (hours - localzone + timezone) * 60 + minutes;
+    return day * MinutesInDay + (hours - localzone + timezone) * MinutesInHour + minutes;
 }
 
 /**
@@ -89,7 +91,7 @@ function getMinutes(time, timezone) {
  * @returns {Object}
  */
 function getAppropriateMoment(schedule, duration, workingHours) {
-    let timezone = parseInt(workingHours.from.match(timeRegex)[4]);
+    let timezone = parseInt(workingHours.from.match(TimeRegex)[4]);
     let points = getSortedTimePoints(schedule, timezone);
     let bankInterval = getBankInterval(workingHours, timezone);
     let freeIntervals = getFreeIntervals(points);
@@ -119,7 +121,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             return !this.exists() ? '' : template
                 .replace('%HH', twoDigit(getHour(this.currentTime)))
                 .replace('%MM', twoDigit(getMinute(this.currentTime)))
-                .replace('%DD', days[getDay(this.currentTime)]);
+                .replace('%DD', Days[getDay(this.currentTime)]);
         },
 
         /**
@@ -131,8 +133,8 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             if (!this.exists()) {
                 return false;
             }
-            if (this.currentTime + 30 + duration <= this.intervals[this.currentIndex].to) {
-                this.currentTime += 30;
+            if (this.currentTime + SkipMinutes + duration <= this.intervals[this.currentIndex].to) {
+                this.currentTime += SkipMinutes;
 
                 return true;
             }
