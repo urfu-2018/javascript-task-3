48f5995dcbe89717071f262fdeccf8e332c9acaf
Max Ratkov <ratkovm8@gmail.com>
2019-01-08 15:32:11 +0500

    fix #1

--- robbery.js
+++ robbery.js
@@ -10,6 +10,20 @@ const MINUTES_IN_DAY = 60 * 24;
 const ROBBERY_DAYS = { 'ПН': 0, 'ВТ': 1, 'СР': 2 };
 const NUMBER_TO_ROBBERY_DAY = { 0: 'ПН', 1: 'ВТ', 2: 'СР' };
 const NEXT_ROBBERY_TIME_OFFSET = 30;
+const TIMESTAMP_COMPARATOR = (a, b) => {
+    if (a[0] < b[0]) {
+        return -1;
+    }
+    if (a[0] > b[0]) {
+        return 1;
+    }
+
+    return 0;
+};
+
+function getTimezone(timeString) {
+    return parseInt(timeString.match(/\+(\d{1,2})/)[1]);
+}
 
 
 /**
@@ -65,7 +79,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
          */
         tryLater: function () {
             if (currentTimestampIndex < robberyTimestamps.length - 1) {
-                currentTimestampIndex += 1;
+                currentTimestampIndex++;
 
                 return true;
             }
@@ -74,9 +88,9 @@ function getAppropriateMoment(schedule, duration, workingHours) {
         }
     };
 
-    function hoursToMinutes(hoursString) {
-        let hours = parseInt(hoursString.substr(0, 2));
-        let minutes = parseInt(hoursString.substr(3, 2));
+    function hoursStringToMinutes(hoursString) {
+        let hours = parseInt(hoursString.substr(0, 2), 10);
+        let minutes = parseInt(hoursString.substr(3, 2), 10);
 
         return hours * 60 + minutes;
     }
@@ -86,12 +100,21 @@ function getAppropriateMoment(schedule, duration, workingHours) {
         let bankSchedule = [];
 
         for (let i = 0; i < Object.keys(ROBBERY_DAYS).length; i++) {
-            let startTimestamp = i * MINUTES_IN_DAY + hoursToMinutes(workingHours.from);
-            let finishTimestamp = i * MINUTES_IN_DAY + hoursToMinutes(workingHours.to);
+            let startTimestamp = i * MINUTES_IN_DAY + hoursStringToMinutes(workingHours.from);
+            let finishTimestamp = i * MINUTES_IN_DAY + hoursStringToMinutes(workingHours.to);
             bankSchedule.push([startTimestamp, finishTimestamp]);
         }
 
         return bankSchedule;
+    }
+
+    function getTimeFromStringInMinutes(timeString, bankTimezone) {
+        const dayNumber = ROBBERY_DAYS[timeString.substr(0, 2)];
+        const robberTimezone = getTimezone(timeString);
+        const timezoneOffset = bankTimezone - robberTimezone;
+        const minutes = hoursStringToMinutes(timeString.substr(3));
+
+        return dayNumber * MINUTES_IN_DAY + minutes + timezoneOffset * 60;
     }
 
     function getRobbersScheduleTimestamps() {
@@ -101,27 +124,14 @@ function getAppropriateMoment(schedule, duration, workingHours) {
         for (let robber in schedule) {
             if (schedule.hasOwnProperty(robber)) {
                 schedule[robber].forEach(timeWindow => {
-                    let startTimestamp = getTimeFromStringInMinutes(timeWindow.from);
-                    let finishTimestamp = getTimeFromStringInMinutes(timeWindow.to);
+                    let startTimestamp = getTimeFromStringInMinutes(timeWindow.from, bankTimezone);
+                    let finishTimestamp = getTimeFromStringInMinutes(timeWindow.to, bankTimezone);
                     robbersSchedule.push([startTimestamp, finishTimestamp]);
                 });
             }
         }
 
         return robbersSchedule;
-
-        function getTimezone(timeString) {
-            return parseInt(timeString.match(/\+(\d{1,2})/)[1]);
-        }
-
-        function getTimeFromStringInMinutes(timeString) {
-            const dayNumber = ROBBERY_DAYS[timeString.substr(0, 2)];
-            const robberTimezone = getTimezone(timeString);
-            const timezoneOffset = bankTimezone - robberTimezone;
-            const minutes = hoursToMinutes(timeString.substr(3));
-
-            return dayNumber * MINUTES_IN_DAY + minutes + timezoneOffset * 60;
-        }
     }
 
     function findPossibleRobberyTimestamps(bankTimestamps, robbersTimestamps) {
@@ -146,16 +156,7 @@ function getAppropriateMoment(schedule, duration, workingHours) {
             }
         });
 
-        robTimestamps.sort((a, b) => {
-            if (a[0] < b[0]) {
-                return -1;
-            }
-            if (a[0] > b[0]) {
-                return 1;
-            }
-
-            return 0;
-        });
+        robTimestamps.sort(TIMESTAMP_COMPARATOR);
 
         return robTimestamps;
     }
@@ -171,12 +172,12 @@ function getAppropriateMoment(schedule, duration, workingHours) {
         return -1;
     }
 
-    function getDay(time) {
-        return NUMBER_TO_ROBBERY_DAY[Math.floor(time / MINUTES_IN_DAY)];
+    function getDay(timestamp) {
+        return NUMBER_TO_ROBBERY_DAY[Math.floor(timestamp / MINUTES_IN_DAY)];
     }
 
-    function getHour(time) {
-        const minutes = time % MINUTES_IN_DAY;
+    function getHour(timestamp) {
+        const minutes = timestamp % MINUTES_IN_DAY;
         let hour = Math.floor(minutes / 60).toString();
         if (hour.length === 1) {
             hour = '0' + hour;
@@ -185,8 +186,8 @@ function getAppropriateMoment(schedule, duration, workingHours) {
         return hour;
     }
 
-    function getMinutes(time) {
-        let minutes = time % MINUTES_IN_DAY;
+    function getMinutes(timestamp) {
+        let minutes = timestamp % MINUTES_IN_DAY;
         minutes = (minutes % 60).toString();
         if (minutes.length === 1) {
             minutes = '0' + minutes;
